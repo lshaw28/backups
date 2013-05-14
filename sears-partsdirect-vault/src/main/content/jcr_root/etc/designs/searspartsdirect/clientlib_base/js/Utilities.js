ec247e0107495a20b7cd507199ef74c41af7078c
Dale Price <dprice@siteworx.com>
2013-05-14 16:10:34 -0500

    Client Library Updates
    Base client library LESS structure work; base JS work.

---
+++ sears-partsdirect-vault/src/main/content/jcr_root/etc/designs/searspartsdirect/clientlib_base/js/Utilities.js
@@ -0,0 +1,15 @@
+/*global $:true, window:true, Class:true */
+(function (window) {
+	"use strict";
+	window.SPDUtils = Class.extend({
+		/**
+		 * @class SPDUtils
+		 * Global utilities and helper methods
+		 */
+		/**
+		 * init: On page load events to fire
+		 */
+		init: function () {
+		}
+	});
+}(window));
