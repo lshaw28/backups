cd5aa4924c8b48a267c00a5d0228f90148cb2640
DoctorAnger <josephlgreenlee@gmail.com>
2013-05-29 16:51:39 -0400

    No longer hardcoded

--- sears-partsdirect-common/src/main/java/com/spd/cq/searspartsdirect/common/tags/GetAssetsTag.java
+++ sears-partsdirect-common/src/main/java/com/spd/cq/searspartsdirect/common/tags/GetAssetsTag.java
@@ -1,13 +1,24 @@
 package com.spd.cq.searspartsdirect.common.tags;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import javax.servlet.jsp.JspException;
 
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ValueMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.day.cq.wcm.api.Page;
+import com.spd.cq.searspartsdirect.common.model.spdasset.BrandModel;
+import com.spd.cq.searspartsdirect.common.model.spdasset.ErrorCodeModel;
+import com.spd.cq.searspartsdirect.common.model.spdasset.HazardModel;
+import com.spd.cq.searspartsdirect.common.model.spdasset.JobCodeModel;
+import com.spd.cq.searspartsdirect.common.model.spdasset.PartTypeModel;
 import com.spd.cq.searspartsdirect.common.model.spdasset.ProductCategoryModel;
+import com.spd.cq.searspartsdirect.common.model.spdasset.TipModel;
+import com.spd.cq.searspartsdirect.common.model.spdasset.WarningModel;
 
 /**
  * Custom tag to draw out a list of SPDAsset objects based on filters
@@ -23,20 +34,30 @@ public class GetAssetsTag extends CQBaseTag {
 	
 	@Override
 	public int doStartTag() throws JspException {
-		ArrayList<ProductCategoryModel> models = new ArrayList<ProductCategoryModel>();
-		ProductCategoryModel chainsaws = new ProductCategoryModel("Chainsaws", "chainsaws description", "/etc/spdAssets/scaffolding/productCategory/chainsaws/jcr:content/image");
-		ProductCategoryModel dishwashers = new ProductCategoryModel("Dishwashers", "dishwashers description", "/etc/spdAssets/scaffolding/productCategory/dishwashers/jcr:content/image");
-		ProductCategoryModel microwaves = new ProductCategoryModel("Microwaves", "microwaves description", "/etc/spdAssets/scaffolding/productCategory/microwaves/jcr:content/image");
-		ProductCategoryModel ranges = new ProductCategoryModel("Ranges", "ranges description", "/etc/spdAssets/scaffolding/productCategory/ranges/jcr:content/image");
-		ProductCategoryModel refrigerators = new ProductCategoryModel("Refrigerators", "refrigerators description", "/etc/spdAssets/scaffolding/productCategory/refrigerators/jcr:content/image");
-		
-		models.add(chainsaws);
-        models.add(dishwashers);
-		models.add(microwaves);
-        models.add(ranges);
-        models.add(refrigerators);  
+		ArrayList<BrandModel> brands = new ArrayList<BrandModel>();
+		ArrayList<ErrorCodeModel> errorCodes = new ArrayList<ErrorCodeModel>();
+		ArrayList<HazardModel> hazards = new ArrayList<HazardModel>();
+		ArrayList<JobCodeModel> jobCodes = new ArrayList<JobCodeModel>();
+		ArrayList<PartTypeModel> partTypes = new ArrayList<PartTypeModel>();
+		ArrayList<ProductCategoryModel> productCategories = new ArrayList<ProductCategoryModel>();
+		ArrayList<TipModel> tips = new ArrayList<TipModel>();
+		ArrayList<WarningModel> warnings = new ArrayList<WarningModel>();
         
-        pageContext.setAttribute("models", models);
+		//Product Categories
+		Resource r = resourceResolver.getResource("/etc/spdAssets/scaffolding/productCategory");
+		if (r != null) {
+			Iterator<Resource> iter = r.listChildren();
+			while (iter.hasNext()) {
+				Resource child = iter.next();
+				Page p = child.adaptTo(Page.class);
+				ValueMap properties = p.getProperties();
+				productCategories.add(new ProductCategoryModel(properties.get("jcr:title",""),
+						properties.get("jcr:description",""),
+						p.getPath() + "/jcr:content/image"));
+			}
+		}
+		
+        pageContext.setAttribute("productCategories", productCategories);
 		return SKIP_BODY;
 	}
 	
