17ba81551fcbc189a59842d433cc94a0d96a0edc
Madhuri Teegavarapu <Madhuri.Teegavarapu@searshc.com>
2013-05-30 11:25:07 -0500

    Most Viewed Guides

--- sears-partsdirect-common/src/main/java/com/spd/cq/searspartsdirect/common/tags/GetMostViewedGuidesTag.java
+++ sears-partsdirect-common/src/main/java/com/spd/cq/searspartsdirect/common/tags/GetMostViewedGuidesTag.java
@@ -2,6 +2,7 @@ package com.spd.cq.searspartsdirect.common.tags;
 
 import java.util.Iterator;
 
+import javax.jcr.PathNotFoundException;
 import javax.jcr.RepositoryException;
 import javax.servlet.jsp.JspException;
 
@@ -9,6 +10,8 @@ import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.jcr.Node;
 
 import com.day.cq.wcm.api.Page;
 import com.day.cq.wcm.api.WCMMode;
@@ -42,12 +45,33 @@ public class GetMostViewedGuidesTag extends CQBaseTag{
 				// TODO Auto-generated catch block
 				//e.printStackTrace();
 			}
+			
+			
+			
 			while (stats.hasNext()) {
 				Object[] res = (Object[]) stats.next();
 				totalPageViews = totalPageViews + Integer.parseInt(res[1].toString());
 			}
 			log.debug(pages.next().getTitle() + totalPageViews);
+			log.debug(statService.getPath());
 		}
+		
+		/*Resource resFolder = resolverPage.getResource("/var/statistics/pages/content/searspartsdirect/en/home/.stats/2013");
+		Node geoNode = resPage.adaptTo(Node.class);*/
+		
+		 Resource r = resolverPage.getResource("/var/statistics/pages/content/searspartsdirect/en/home/.stats/2013");
+		Node node = r.adaptTo(Node.class);
+		Iterator properties = null;
+		try {
+			properties = node.getProperties();
+		} catch (RepositoryException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		while (properties.hasNext()){
+			
+		}
+		
 
 		return SKIP_BODY;
 	}
