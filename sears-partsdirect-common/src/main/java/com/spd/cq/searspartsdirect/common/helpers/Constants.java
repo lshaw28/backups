7f711c391ca7af19dc1f809799f409e52c7b8fb8
Vivek Sharma <vivek.sharma@searshc.com>
2013-05-30 16:24:41 -0500

    [Code clean up] - added variables in Constants.java and using it in the code.

--- sears-partsdirect-common/src/main/java/com/spd/cq/searspartsdirect/common/helpers/Constants.java
+++ sears-partsdirect-common/src/main/java/com/spd/cq/searspartsdirect/common/helpers/Constants.java
@@ -12,6 +12,9 @@ public final class Constants {
 	public static final String PARTS_REQ_DEF_STICKY_LINK = ident("Parts required");
 	public static final String TOOLS_REQ_DEF_STICKY_LINK = ident("Tools required");
 	public static final String COMMENTS_STICKY_LINK_PREFIX = ident("Comments");
+	
+	public static final String RECENTLY_VIEWED_MODEL_COOKIE = "recentlyViewedModels";
+	public static final String RECENTLY_VIEWED_PART_COOKIE = "recentlyViewedParts";
 
 	private final static <T> T ident(T t) {
 		return t;
