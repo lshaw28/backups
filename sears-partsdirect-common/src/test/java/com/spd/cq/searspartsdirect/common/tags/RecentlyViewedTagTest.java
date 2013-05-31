f78d1640563ca91baa97ee68e4bbd85e3115fff5
Vivek Sharma <vivek.sharma@searshc.com>
2013-05-31 17:28:13 -0500

    [Test cases] - fixed the testcases not running issue. Deleting the older testcases and committing the fresh testcase with new package structure.

--- sears-partsdirect-common/src/test/java/com/spd/cq/searspartsdirect/common/tags/RecentlyViewedTagTest.java
+++ sears-partsdirect-common/src/test/java/com/spd/cq/searspartsdirect/common/tags/RecentlyViewedTagTest.java
@@ -21,15 +21,15 @@ public class RecentlyViewedTagTest extends TestCase {
 	HttpServletRequest request;
 	PageContext pageContext;
 	RecentlyViewedTag recentlyViewedTag;
-	
+
 	@Override
 	protected void setUp() {
 		recentlyViewedFixture = new RecentlyViewedFixture();
 		request = Mockito.mock(HttpServletRequest.class);
 		pageContext = Mockito.mock(PageContext.class);
 		recentlyViewedTag = new RecentlyViewedTag();
-    }
-	
+	}
+
 	@Test
 	public void testWithNoMatchingCookie() throws JspException {
 		Mockito.when(pageContext.getRequest()).thenReturn(request);
@@ -38,13 +38,13 @@ public class RecentlyViewedTagTest extends TestCase {
 		recentlyViewedTag.setPageContext(pageContext);
 		recentlyViewedTag.doStartTag();
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	@Test
 	public void testWithMatchingCookies() throws JspException {
 		LinkedList<ModelCookieModel> modelList = recentlyViewedFixture.getModelList();
 		LinkedList<PartCookieModel> partList = recentlyViewedFixture.getPartList();
-		
+
 		Mockito.when(pageContext.getRequest()).thenReturn(request);
 		Mockito.when(request.getCookies()).thenReturn(recentlyViewedFixture.getPartAndModelCookie());
 		Mockito.when(pageContext.getAttribute("rvModelList")).thenReturn(modelList);
@@ -52,14 +52,14 @@ public class RecentlyViewedTagTest extends TestCase {
 
 		recentlyViewedTag.setPageContext(pageContext);
 		recentlyViewedTag.doStartTag();
-		
+
 		LinkedList<ModelCookieModel> actualModelList = (LinkedList<ModelCookieModel>) pageContext.getAttribute("rvModelList");
 		Assert.assertNotNull(actualModelList);
 		Assert.assertTrue(actualModelList.size() > 0);
 		Assert.assertEquals("3333", actualModelList.get(0).getItemName());
 		Assert.assertEquals("Some description", actualModelList.get(0).getItemDescription());
-		Assert.assertEquals("http://www.SPD.com/ModelUrl", actualModelList.get(0).getItemURL());
-		
+		Assert.assertEquals("http://www.SPD.com/ModelUrl",actualModelList.get(0).getItemURL());
+
 		LinkedList<PartCookieModel> actualPartList = (LinkedList<PartCookieModel>) pageContext.getAttribute("rvPartList");
 		Assert.assertNotNull(actualPartList);
 		Assert.assertTrue(actualPartList.size() > 0);
