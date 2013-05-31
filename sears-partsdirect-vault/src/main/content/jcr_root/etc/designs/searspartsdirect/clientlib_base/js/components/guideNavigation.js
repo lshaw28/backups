203fd7417e71a144ddf87a31522a78ef1fcb0515
Dale Price <dprice@siteworx.com>
2013-05-31 15:43:48 -0500

    guideNavigation ClientLibs Tidy Up and JavaScript Class Stub

---
+++ sears-partsdirect-vault/src/main/content/jcr_root/etc/designs/searspartsdirect/clientlib_base/js/components/guideNavigation.js
@@ -0,0 +1,17 @@
+/*global $:true, window:true, Class:true */
+(function (window) {
+	/**
+	 * @class guideNavigation
+	 * guideNavigation component - creates anchors and maintains sizing
+	 */
+	"use strict";
+	window.guideNavigation = Class.extend({
+		/**
+		 * Initializes guideNavigation class
+		 */
+		init: function (el) {
+			// Parameters
+			this.el = el;
+		}
+	});
+}(window));
