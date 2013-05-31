99abe4607b4d5206817f97dde0a386d100302528
Madhuri Teegavarapu <Madhuri.Teegavarapu@searshc.com>
2013-05-30 22:36:25 -0500

    Most Viewed Guides

---
+++ sears-partsdirect-common/src/main/java/com/spd/cq/searspartsdirect/common/helpers/PageStatistics.java
@@ -0,0 +1,32 @@
+package com.spd.cq.searspartsdirect.common.helpers;
+
+public class PageStatistics {
+	private String title;
+	private String pagePath;
+	private Long views;
+		
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getPagePath() {
+		return pagePath;
+	}
+	
+	public void setPagePath(String pagePath) {
+		this.pagePath = pagePath;
+	}
+	
+	public Long getViews() {
+		return views;
+	}
+	
+	public void setViews(Long views) {
+		this.views = views;
+	}
+
+}
--- sears-partsdirect-common/src/main/java/com/spd/cq/searspartsdirect/common/tags/GetMostViewedGuidesTag.java
+++ sears-partsdirect-common/src/main/java/com/spd/cq/searspartsdirect/common/tags/GetMostViewedGuidesTag.java
@@ -1,8 +1,12 @@
 package com.spd.cq.searspartsdirect.common.tags;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 import javax.jcr.PathNotFoundException;
+import javax.jcr.PropertyIterator;
+import javax.jcr.Property;
 import javax.jcr.RepositoryException;
 import javax.servlet.jsp.JspException;
 
@@ -19,6 +23,8 @@ import com.day.cq.wcm.core.stats.PageViewStatistics;
 import com.day.cq.wcm.core.stats.PageViewReport;
 import com.day.cq.statistics.StatisticsService;
 
+import com.spd.cq.searspartsdirect.common.helpers.PageStatistics;
+
 public class GetMostViewedGuidesTag extends CQBaseTag{
 	
 	protected static Logger log = LoggerFactory.getLogger(GetMostViewedGuidesTag.class);
@@ -26,53 +32,40 @@ public class GetMostViewedGuidesTag extends CQBaseTag{
 	@Override
 	public int doStartTag() throws JspException {
 		Iterator<Page> pages = null;
-		StatisticsService statService = sling.getService(StatisticsService.class);
+		List<PageStatistics> pageList = new ArrayList<PageStatistics>();
 		ResourceResolver resolverPage = slingRequest.getResourceResolver();
-		Resource resPage = resolverPage.getResource("/content/geometrixx");
+		Resource resPage = resolverPage.getResource("/content/searspartsdirect/en/home");
 		
-		Page geoPages = resPage.adaptTo(Page.class);
-		pages = geoPages.listChildren();
-		log.debug("CHILD PAGES : ");
+		Page repairPages = resPage.adaptTo(Page.class);
+		pages = repairPages.listChildren();
 		while (pages.hasNext()){
-			PageViewReport report = new PageViewReport(statService.getPath() + "/pages", pages.next(), WCMMode.EDIT);
-			report.setPeriod(30);
-			log.debug(Integer.toString(report.getPeriod()));
-			Iterator stats = null;
-			int totalPageViews = 0;
+			Page childPages = pages.next();
+			PageStatistics pageStats = new PageStatistics();
+			pageStats.setTitle(childPages.getTitle());
+			pageStats.setPagePath(childPages.getPath());
+			
 			try {
-				stats = statService.runReport(report);
-			} catch (RepositoryException e) {
-				// TODO Auto-generated catch block
-				//e.printStackTrace();
-			}
-			
-			
-			
-			while (stats.hasNext()) {
-				Object[] res = (Object[]) stats.next();
-				totalPageViews = totalPageViews + Integer.parseInt(res[1].toString());
-			}
-			log.debug(pages.next().getTitle() + totalPageViews);
-			log.debug(statService.getPath());
-		}
-		
-		/*Resource resFolder = resolverPage.getResource("/var/statistics/pages/content/searspartsdirect/en/home/.stats/2013");
-		Node geoNode = resPage.adaptTo(Node.class);*/
-		
-		 Resource r = resolverPage.getResource("/var/statistics/pages/content/searspartsdirect/en/home/.stats/2013");
-		Node node = r.adaptTo(Node.class);
-		Iterator properties = null;
-		try {
-			properties = node.getProperties();
-		} catch (RepositoryException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		while (properties.hasNext()){
-			
-		}
-		
+				Resource r = resolverPage.getResource("/var/statistics/pages/content/searspartsdirect/en/home" + childPages.getName() + "/.stats/2013");
+				Node node = r.adaptTo(Node.class);
+				PropertyIterator properties = null;
+				properties = node.getProperties();
 
+				while (properties.hasNext()){
+					Property prop=properties.nextProperty();
+					if ("views".equalsIgnoreCase(prop.getName())){
+						pageStats.setViews(prop.getValue().getLong());
+					}
+				}
+				
+				} catch (RepositoryException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			pageList.add(pageStats);
+			
+		}
+		
+		
 		return SKIP_BODY;
 	}
  
