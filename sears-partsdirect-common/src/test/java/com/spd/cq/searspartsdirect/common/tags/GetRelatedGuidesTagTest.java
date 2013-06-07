41e89f2255d5d8e028d5ff16383ade3f73a683c7
David Martinez <dmartinez@siteworx.com>
2013-06-07 08:50:39 -0500

    created test classes for related articles and related guides custom tags

---
+++ sears-partsdirect-common/src/test/java/com/spd/cq/searspartsdirect/common/tags/GetRelatedGuidesTagTest.java
@@ -0,0 +1,54 @@
+package com.spd.cq.searspartsdirect.common.tags;
+
+import java.util.ArrayList;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.PageContext;
+import junit.framework.TestCase;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import com.spd.cq.searspartsdirect.common.model.RelatedGuideModel;
+
+public class GetRelatedGuidesTagTest extends TestCase {
+	
+	String categoryPathAttribute;
+	GetRelatedArticlesTag tag;
+	ArrayList<RelatedGuideModel> guides;
+
+	PageContext pageContext;
+	ResourceResolver resourceResolver;
+	
+	@Override
+	protected void setUp() {
+		pageContext = Mockito.mock(PageContext.class);
+		tag = new GetRelatedArticlesTag();
+		guides = new ArrayList<RelatedGuideModel>();
+
+	    // dummy data
+		RelatedGuideModel guide1 = new RelatedGuideModel("url1.html", "imagePath1", "title1");
+		RelatedGuideModel guide2 = new RelatedGuideModel("url2.html", "imagePath2", "title2");
+		RelatedGuideModel guide3 = new RelatedGuideModel("url3.html", "imagePath3", "title3");
+		RelatedGuideModel guide4 = new RelatedGuideModel("url4.html", "imagePath4", "title4");
+
+		guides.add(guide1);
+		guides.add(guide2);				
+		guides.add(guide3);			
+		guides.add(guide4);
+		
+	}
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void testRelatedArticles() throws JspException {
+		ArrayList<RelatedGuideModel> guidesCheck;
+		Mockito.when(pageContext.getAttribute("guides")).thenReturn(guides);
+		tag.doStartTag();
+		guidesCheck = (ArrayList<RelatedGuideModel>) pageContext.getAttribute("guides");
+		for (int i=0; i < guidesCheck.size(); i++) {
+			assertEquals(guidesCheck.get(i).getUrl(), guides.get(i).getUrl());
+			assertEquals(guidesCheck.get(i).getImagePath(), guides.get(i).getImagePath());
+			assertEquals(guidesCheck.get(i).getTitle(), guides.get(i).getTitle());
+		}
+	}
+}
