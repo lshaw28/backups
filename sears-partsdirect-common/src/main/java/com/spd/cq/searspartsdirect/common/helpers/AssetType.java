4f3b063ea283641b79a4d29c3bc7bbd86a44f4a4
Benjamin Zethmayr <bzethmayr@siteworx.com>
2013-06-17 15:44:22 -0500

    Added attributes to AssetType for mixedCaseName and modelClass, and test case to verify.

--- sears-partsdirect-common/src/main/java/com/spd/cq/searspartsdirect/common/helpers/AssetType.java
+++ sears-partsdirect-common/src/main/java/com/spd/cq/searspartsdirect/common/helpers/AssetType.java
@@ -1,5 +1,37 @@
 package com.spd.cq.searspartsdirect.common.helpers;
 
+import com.spd.cq.searspartsdirect.common.model.spdasset.*;
+
 public enum AssetType {
-	BRAND, ERRORCODE, HAZARD, JOBCODE, PARTTYPE, PRODUCTCATEGORY, TIP, WARNING, SYMPTOM, AUTHOR
+	BRAND(BrandModel.class), 
+	ERRORCODE(ErrorCodeModel.class,"errorCode"), 
+	HAZARD(HazardModel.class), 
+	JOBCODE(JobCodeModel.class,"jobCode"), 
+	PARTTYPE(PartTypeModel.class,"partType"), 
+	PRODUCTCATEGORY(ProductCategoryModel.class,"productCategory"), 
+	TIP(TipModel.class), 
+	WARNING(WarningModel.class), 
+	SYMPTOM(SymptomModel.class), 
+	AUTHOR(AuthorModel.class);
+	
+	private final Class<?> modelClass;
+	private final String mixedCaseName;
+	
+	AssetType(final Class<?> modelClass) {
+		this.modelClass = modelClass;
+		mixedCaseName = this.name().toLowerCase();
+	}
+	
+	AssetType(final Class<?> modelClass, final String mixedCaseName) {
+		this.modelClass = modelClass;
+		this.mixedCaseName = mixedCaseName;
+	}
+	
+	public Class<?> getModelClass() {
+		return modelClass;
+	}
+	
+	public String getMixedCaseName() {
+		return mixedCaseName;
+	}
 }
