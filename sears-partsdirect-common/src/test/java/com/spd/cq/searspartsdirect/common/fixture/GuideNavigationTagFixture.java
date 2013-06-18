73bdf723128db93ad9adb9cdc1b97edc1d96ad0e
Benjamin Zethmayr <bzethmayr@siteworx.com>
2013-06-18 01:24:15 -0500

    Test case for GuideNavigationTag now covers all default functionality

--- sears-partsdirect-common/src/test/java/com/spd/cq/searspartsdirect/common/fixture/GuideNavigationTagFixture.java
+++ sears-partsdirect-common/src/test/java/com/spd/cq/searspartsdirect/common/fixture/GuideNavigationTagFixture.java
@@ -1,6 +1,8 @@
 package com.spd.cq.searspartsdirect.common.fixture;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 import javax.jcr.Node;
 import javax.jcr.Property;
@@ -9,12 +11,17 @@ import javax.jcr.Session;
 import javax.jcr.Value;
 import javax.jcr.nodetype.PropertyDefinition;
 
+import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.hamcrest.Matcher;
 import org.mockito.Mockito;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 
+import com.adobe.cq.social.commons.CommentSystem;
 import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.api.WCMMode;
 import com.spd.cq.searspartsdirect.common.helpers.Constants;
 
 import static org.mockito.Mockito.*;
@@ -22,16 +29,20 @@ import static org.mockito.Mockito.*;
 public class GuideNavigationTagFixture {
 	
 	private final static String jumpToValue = "Jump to...";
-	
+	private final static String testPagePath = "/content/searspartsdirect/en/test";
+	private final static String h2Contents = "Before you begin";
 	private Resource pageContentResource;
+	
 	private Node pageNode;
 	private Node setupNode;
 	private Property sectionsProperty;
 	private Property jumpProperty;
 	
-	public GuideNavigationTagFixture(Page currentPage) throws Exception {
+	
+	public GuideNavigationTagFixture(Page currentPage, SlingHttpServletRequest slingRequest, ResourceResolver resourceResolver) throws Exception {
 		pageContentResource = mock(Resource.class);
 		when(currentPage.getContentResource()).thenReturn(pageContentResource);
+		when(currentPage.getPath()).thenReturn(getCurrentPagePath());
 		pageNode = mock(Node.class);
 		when(pageContentResource.adaptTo(Node.class)).thenReturn(pageNode);
 		
@@ -79,15 +90,60 @@ public class GuideNavigationTagFixture {
 		jumpProperty = mock(Property.class);
 		when(pageNode.getProperty(Constants.GUIDE_NAV_JUMPTO_TEXT_PAGE_ATTR)).thenReturn(jumpProperty);
 		when(jumpProperty.getString()).thenReturn(getJumpToValue());
-		
+		/* // included as example. will remove on next commit.
+		when(slingRequest.getAttribute(any(String.class))).thenAnswer(new Answer<Void>() {
+
+			public Void answer(InvocationOnMock invocation) throws Throwable {
+				// TODO Auto-generated method stub
+				throw new RuntimeException((String)invocation.getArguments()[0]);
+			}
+			
+		});
+		*/
+		when(slingRequest.getAttribute("com.day.cq.wcm.api.WCMMode")).thenReturn(WCMMode.EDIT);
 		Resource parsysResource = mock(Resource.class);
 		when(currentPage.getContentResource(Constants.GUIDE_TOP_PARSYS_NAME)).thenReturn(parsysResource);
-		@SuppressWarnings("unchecked")
-		Iterator<Resource> parsysChildren = mock(Iterator.class);
-		when(parsysResource.listChildren()).thenReturn(parsysChildren);
+		final List<Resource> parsysChildren = new ArrayList<Resource>();
+		Resource instructionsComponent = mock(Resource.class);
+		when(instructionsComponent.getResourceType()).thenReturn(Constants.INSTRUCTIONS_COMPONENT);
+		parsysChildren.add(instructionsComponent);
+		Resource textResource = mock(Resource.class);
+		when(textResource.getResourceType()).thenReturn(Constants.TEXT_COMPONENT);
+		Node textNode = mock(Node.class);
+		when(textResource.adaptTo(Node.class)).thenReturn(textNode);
+		Property textProperty = mock(Property.class);
+		when(textNode.getProperty(Constants.GUIDE_TEXT_LABEL_PROP)).thenReturn(textProperty);
+		when(textProperty.getString()).thenReturn("<h2>"+getTextHeader()+"</h2><p>Read the source code</p>");
+		parsysChildren.add(textResource);
+		when(parsysResource.listChildren()).thenAnswer(new Answer<Iterator<Resource>>() {
+
+			public Iterator<Resource> answer(InvocationOnMock invocation)
+					throws Throwable {
+				return parsysChildren.iterator();
+			}
+			
+		});
+		Resource commentsResource = mock(Resource.class);
+		when(resourceResolver.resolve(Constants.USERGEN_ROOT+getCurrentPagePath()+Constants.GUIDE_COMMENTS_PATH)).thenReturn(commentsResource);
+		CommentSystem commentSystem = mock(CommentSystem.class);
+		when(commentsResource.adaptTo(CommentSystem.class)).thenReturn(commentSystem);
+		when(commentSystem.countComments()).thenReturn(getCommentCount());
+	}
+	
+	public String getCurrentPagePath() {
+		return testPagePath;
 	}
 	
 	public String getJumpToValue() {
 		return jumpToValue;
+	}
+
+	public String getTextHeader() {
+		return h2Contents;
+	}
+
+	public int getCommentCount() {
+		// TODO Auto-generated method stub
+		return 301;
 	}
 }
--- sears-partsdirect-common/src/test/java/com/spd/cq/searspartsdirect/common/tags/GuideNavigationTagTest.java
+++ sears-partsdirect-common/src/test/java/com/spd/cq/searspartsdirect/common/tags/GuideNavigationTagTest.java
@@ -2,6 +2,8 @@ package com.spd.cq.searspartsdirect.common.tags;
 
 import java.util.List;
 
+import javax.servlet.jsp.tagext.TagSupport;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -20,7 +22,7 @@ public class GuideNavigationTagTest extends MocksTag {
 	protected void setUp() throws Exception {
 		super.setUp();
 		
-		fixture = new GuideNavigationTagFixture(currentPage);
+		fixture = new GuideNavigationTagFixture(currentPage,slingRequest,resourceResolver);
 		guideNavigationTag = new GuideNavigationTag();
 	}
 
@@ -29,9 +31,28 @@ public class GuideNavigationTagTest extends MocksTag {
 	public void testDoStartTag() {
 		try {
 			guideNavigationTag.setPageContext(pageContext);
-			guideNavigationTag.doStartTag();
+			int startResult = guideNavigationTag.doStartTag();
+			assertThat(startResult,is(TagSupport.SKIP_BODY));
+			int endResult = guideNavigationTag.doEndTag();
+			assertThat(endResult,is(TagSupport.EVAL_PAGE));
 			assertThat((String)pageContext.getAttribute(Constants.GUIDE_NAV_JUMPTO_TEXT_PAGE_ATTR),is(fixture.getJumpToValue()));
-			assertThat((List<List<String>>)pageContext.getAttribute(Constants.GUIDE_NAV_SECTIONS_PAGE_ATTR),instanceOf(List.class));
+			List<List<String>> result = (List<List<String>>)pageContext.getAttribute(Constants.GUIDE_NAV_SECTIONS_PAGE_ATTR);
+			assertThat(result,instanceOf(List.class));
+			assertThat(result,hasSize(5));
+			for (List<String> link : result) {
+				assertThat(link,isA(List.class));
+				assertThat(link,hasSize(2));
+				assertThat(link.get(0),not(nullValue()));
+				assertThat(link.get(0),not(""));
+				assertThat(link.get(1),not(nullValue()));
+				assertThat(link.get(1),not(""));
+				assertThat(link.get(1),containsString("_"));
+			}
+			assertThat(result.get(0).get(0),is(Constants.TOOLS_REQ_DEF_GUIDE_NAV_LINK));
+			assertThat(result.get(1).get(0),is(Constants.PARTS_REQ_DEF_GUIDE_NAV_LINK));
+			assertThat(result.get(2).get(0),is(Constants.INSTRUCTIONS_DEF_GUIDE_NAV_LINK));
+			assertThat(result.get(3).get(0),is(fixture.getTextHeader()));
+			assertThat(result.get(4).get(0),is(Constants.COMMENTS_GUIDE_NAV_LINK_PREFIX+" ("+fixture.getCommentCount()+")"));
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
