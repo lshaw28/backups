909e9b0485997f61e44fadd94eccdfa39145155c
DoctorAnger <josephlgreenlee@gmail.com>
2013-06-19 11:41:49 -0400

    Merging with ben's, fixing some extra includes

--- sears-partsdirect-common/src/test/java/com/spd/cq/searspartsdirect/common/fixture/ImageApprovalWorkflowProcessFixture.java
+++ sears-partsdirect-common/src/test/java/com/spd/cq/searspartsdirect/common/fixture/ImageApprovalWorkflowProcessFixture.java
@@ -3,27 +3,23 @@ package com.spd.cq.searspartsdirect.common.fixture;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import javax.jcr.Item;
 import javax.jcr.Node;
 import javax.jcr.PathNotFoundException;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
 import javax.jcr.Workspace;
 
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
-
 import com.adobe.granite.workflow.WorkflowSession;
 import com.adobe.granite.workflow.exec.WorkItem;
 import com.adobe.granite.workflow.exec.WorkflowData;
 import com.adobe.granite.workflow.metadata.MetaDataMap;
-import com.spd.cq.searspartsdirect.common.foundation.ImageApprovalWorkflowProcess;
 import com.spd.cq.searspartsdirect.common.helpers.Constants;
 
 public class ImageApprovalWorkflowProcessFixture {
 	private WorkItem item;
 	private WorkflowSession session;
 	private MetaDataMap args;
+	
 	private WorkflowData workflowData;
 	private Session jcrSession;
 
@@ -34,39 +30,7 @@ public class ImageApprovalWorkflowProcessFixture {
 		item = mock(WorkItem.class);
 		session = mock(WorkflowSession.class);
 		args = mock(MetaDataMap.class);
-		WorkflowData workflowData = mock(WorkflowData.class);
-		when(item.getWorkflowData()).thenReturn(workflowData);
-		when(workflowData.getPayloadType()).thenReturn(ImageApprovalWorkflowProcess.TYPE_JCR_PATH);
-		String payload = "payload";
-		when(workflowData.getPayload()).thenReturn(payload);
-		Session jcrSession = mock(Session.class);
-		when(session.adaptTo(Session.class)).thenReturn(jcrSession);
-		Workspace workspace = mock(Workspace.class);
-		when(jcrSession.getWorkspace()).thenReturn(workspace);
-		Node node = mock(Node.class);
-		try {
-			when(node.getPath()).thenReturn(Constants.DAM_PENDING_APPROVAL_PATH + "/some/destination");
-			when(jcrSession.getItem(payload.toString())).thenReturn(node);
-		}
-		catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
-
-	public WorkItem getItem() {
-		return item;
-	}
-
-	public void setItem(WorkItem item) {
-		this.item = item;
-	}
-
-	public WorkflowSession getSession() {
-		return session;
-	}
-
-	public void setSession(WorkflowSession session) {
-		this.session = session;
+		
 		// WorkflowData workflowData = item.getWorkflowData();
 		workflowData = mock(WorkflowData.class);
 		when(item.getWorkflowData()).thenReturn(workflowData);
@@ -105,11 +69,7 @@ public class ImageApprovalWorkflowProcessFixture {
 	
 	public void setUpException() throws PathNotFoundException, RepositoryException {
 		setUpMinConditions();
-		when(jcrSession.getItem(PATH)).thenAnswer(new Answer<Item>() {
-			public Item answer(InvocationOnMock invocation) throws Throwable {
-				throw new RepositoryException();
-			}
-		});
+		when(jcrSession.getItem(PATH)).thenThrow(new RepositoryException());
 	}
 	
 	public WorkItem getTestItem() {
@@ -124,7 +84,5 @@ public class ImageApprovalWorkflowProcessFixture {
 		return args;
 	}
 
-	public void setArgs(MetaDataMap args) {
-		this.args = args;
-	}
+	
 }
