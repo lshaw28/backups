6e6ba75494cc73f4856005af374d481b4981d721
DoctorAnger <josephlgreenlee@gmail.com>
2013-06-21 16:57:54 -0400

    Test clsases for TagGenerationServlet and ImageDenialWorkflowProcess

--- sears-partsdirect-common/src/test/java/com/spd/cq/searspartsdirect/common/fixture/ImageDenialWorkflowProcessFixture.java
+++ sears-partsdirect-common/src/test/java/com/spd/cq/searspartsdirect/common/fixture/ImageDenialWorkflowProcessFixture.java
@@ -4,6 +4,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import javax.jcr.Node;
+import javax.jcr.RepositoryException;
 import javax.jcr.Session;
 import javax.jcr.Workspace;
 
@@ -19,14 +20,18 @@ public class ImageDenialWorkflowProcessFixture {
 	private WorkItem item;
 	private WorkflowSession session;
 	private MetaDataMap args;
+	private WorkflowData workflowData;
 	
 	public ImageDenialWorkflowProcessFixture() {
 		item = mock(WorkItem.class);
 		session = mock(WorkflowSession.class);
 		args = mock(MetaDataMap.class);
 		
-		WorkflowData workflowData = mock(WorkflowData.class);
+		workflowData = mock(WorkflowData.class);
 		when(item.getWorkflowData()).thenReturn(workflowData);
+	}
+
+	public void setUpCorrectPayload() {
 		when(workflowData.getPayloadType()).thenReturn(ImageDenialWorkflowProcess.TYPE_JCR_PATH);
 		String payload = "payload";
 		when(workflowData.getPayload()).thenReturn(payload);
@@ -43,28 +48,54 @@ public class ImageDenialWorkflowProcessFixture {
 			throw new RuntimeException(e);
 		}
 	}
+	
+	public void setUpIncorrectPayload() {
+		when(workflowData.getPayloadType()).thenReturn("Not Type JCR Path");
+	}
+	
+	public void setUpNullPayload() {
+		when(workflowData.getPayloadType()).thenReturn(ImageDenialWorkflowProcess.TYPE_JCR_PATH);
+		String payload = "payload";
+		when(workflowData.getPayload()).thenReturn(payload);
+		Session jcrSession = mock(Session.class);
+		when(session.adaptTo(Session.class)).thenReturn(jcrSession);
+		Workspace workspace = mock(Workspace.class);
+		when(jcrSession.getWorkspace()).thenReturn(workspace);
+		try {
+			when(jcrSession.getItem(payload.toString())).thenReturn(null);
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public void setUpExceptions() {
+		when(workflowData.getPayloadType()).thenReturn(ImageDenialWorkflowProcess.TYPE_JCR_PATH);
+		String payload = "payload";
+		when(workflowData.getPayload()).thenReturn(payload);
+		Session jcrSession = mock(Session.class);
+		when(session.adaptTo(Session.class)).thenReturn(jcrSession);
+		Workspace workspace = mock(Workspace.class);
+		when(jcrSession.getWorkspace()).thenReturn(workspace);
+		Node node = mock(Node.class);
+		try {
+			when(node.getPath()).thenThrow(new RepositoryException());
+			when(jcrSession.getItem(payload.toString())).thenReturn(node);
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
 
 	public WorkItem getItem() {
 		return item;
 	}
-
-	public void setItem(WorkItem item) {
-		this.item = item;
-	}
-
+	
 	public WorkflowSession getSession() {
 		return session;
 	}
 
-	public void setSession(WorkflowSession session) {
-		this.session = session;
-	}
-
 	public MetaDataMap getArgs() {
 		return args;
-	}
-
-	public void setArgs(MetaDataMap args) {
-		this.args = args;
 	}
 }
