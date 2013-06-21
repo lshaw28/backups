435de485346b65efd4ee65e48e5737906794bc5b
DoctorAnger <josephlgreenlee@gmail.com>
2013-06-21 13:29:22 -0400

    Initial test cases for TagGeneration, still have to do exceptions

---
+++ sears-partsdirect-common/src/test/java/com/spd/cq/searspartsdirect/common/fixture/TagGenerationServletFixture.java
@@ -0,0 +1,110 @@
+package com.spd.cq.searspartsdirect.common.fixture;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.request.RequestParameter;
+import org.apache.sling.api.resource.ResourceResolver;
+
+import com.day.cq.tagging.Tag;
+import com.day.cq.tagging.TagManager;
+
+
+public class TagGenerationServletFixture {
+	private SlingHttpServletRequest request;
+	private SlingHttpServletResponse response;
+	private TagManager tm;
+	private ResourceResolver resourceResolver;
+	private String csv;
+	private RequestParameter requestParameter;
+	
+	public TagGenerationServletFixture() {
+		request = mock(SlingHttpServletRequest.class);
+		response = mock(SlingHttpServletResponse.class);
+		resourceResolver = mock(ResourceResolver.class);
+		when(request.getResourceResolver()).thenReturn(resourceResolver);
+		requestParameter = mock(RequestParameter.class);
+		when(request.getRequestParameter("file")).thenReturn(requestParameter);
+	}
+	
+	public void setUpNamespace() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Product Categories,a,a,a\r\n");
+		sb.append("Product Categories,b,b,n/a\r\n");
+		sb.append("Product Categories,b,b,b\r\n");
+		sb.append("Not Product Categories,a,b,n/a");
+		csv = sb.toString();
+		
+		InputStream inputStream = new ByteArrayInputStream(csv.getBytes());
+		try {
+			when(requestParameter.getInputStream()).thenReturn(inputStream);
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		
+		tm = mock(TagManager.class);
+		when(resourceResolver.adaptTo(TagManager.class)).thenReturn(tm);
+		Tag topLevel = mock(Tag.class);
+		Tag parentCategories = mock(Tag.class);
+		Tag subcategories = mock(Tag.class);
+		when(tm.resolve("searspartsdirect:")).thenReturn(topLevel);
+		when(tm.resolve("searspartsdirect:parent_categories")).thenReturn(parentCategories);
+		when(tm.resolve("searspartsdirect:subcategories")).thenReturn(subcategories);
+		
+		Tag check = mock(Tag.class);
+		when(tm.resolve("searspartsdirect:parent_categories/a")).thenReturn(check);
+		when(tm.resolve("searspartsdirect:parent_categories/b")).thenReturn(null);
+		try {
+			when(tm.createTag("searspartsdirect:parent_categories/b", "b", "", true)).thenReturn(check);
+			when(tm.createTag("searspartsdirect:subcategories/b", "b", "", true)).thenReturn(check);
+			when(check.getTagID()).thenReturn("b");
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		when(tm.resolve("searspartsdirect:subcategories/a")).thenReturn(check);
+		when(tm.resolve("searspartsdirect:subcategories/b")).thenReturn(null);
+	}
+
+	public void setUpNullNamespace() {
+		csv = "";
+		
+		InputStream inputStream = new ByteArrayInputStream(csv.getBytes());
+		try {
+			when(requestParameter.getInputStream()).thenReturn(inputStream);
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		
+		tm = mock(TagManager.class);
+		when(resourceResolver.adaptTo(TagManager.class)).thenReturn(tm);
+		Tag topLevel = mock(Tag.class);
+		Tag parentCategories = mock(Tag.class);
+		Tag subcategories = mock(Tag.class);
+		when(tm.resolve("searspartsdirect:")).thenReturn(null);
+		when(tm.resolve("searspartsdirect:parent_categories")).thenReturn(null);
+		when(tm.resolve("searspartsdirect:subcategories")).thenReturn(null);
+		try {
+			when(tm.createTag("searspartsdirect:", "Sears Parts Direct", "Namespace for Sears Parts Direct")).thenReturn(topLevel);
+			when(tm.createTag("searspartsdirect:parent_categories", "Parent Categories", "Tag for Parent Categories")).thenReturn(parentCategories);
+			when(tm.createTag("searspartsdirect:subcategories", "Subcategories", "Tag for Subcategories")).thenReturn(subcategories);
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public SlingHttpServletRequest getRequest() {
+		return request;
+	}
+	public SlingHttpServletResponse getResponse() {
+		return response;
+	}
+}
