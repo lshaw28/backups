960d6d2186cedf06ce4a276acd8aae3f383f5087
Matt Lo <mlo@siteworx.com>
2013-06-24 11:24:33 -0500

    added timer wrapper for setTimeout and setInterval

---
+++ sears-partsdirect-vault/src/main/content/jcr_root/etc/designs/searspartsdirect/clientlib_base/js/util/Timer.js
@@ -0,0 +1,66 @@
+NS('shc.pd.base.util').Timer = Class.extend(function () {
+	'use strict';
+	return {
+		/**
+		 * @constructor
+		 * @param {number} interval Number of milliseconds between each iteration
+		 * @param {boolean} singleUse Determines to fire one or many iterations
+		 */
+		init: function (interval, singleUse) {
+			this.interval = interval;
+			this.singleUse = singleUse;
+			this.session = null; // stores Interval session
+			this.tickAction = function () {}; // stores executable code as a functional expression
+		},
+		/**
+		 * Starts iteration
+		 * @return {undefined}
+		 */
+		start: function () {
+			if (this.singleUse === true) {
+				// single iteration
+				this.session = setTimeout(this.tickAction, this.interval);
+			} else {
+				// multiple iterations
+				this.session = setInterval(this.tickAction, this.interval);
+			}
+		},
+		/**
+		 * Stop iteration
+		 * @param {boolean} invokeLastTickAction Fires last tick if set to true
+		 * @return {undefined}
+		 */
+		stop: function (invokeLastTickAction) {
+			// check for multiple iterations
+			if (this.singleUse === true) {
+				throw new Error('Cannot use `stop` on `setTimeout`s');
+			}
+			
+			// set default
+			invokeLastTickAction = invokeLastTickAction || false;
+			// stop timer session
+			clearInterval(this.session);
+			
+			// optional fire
+			if (invokeLastTickAction === true) {
+				this.tickAction();
+			}
+		},
+		/**
+		 * Stops and starts timer
+		 * @return {undefined}
+		 */
+		reset: function () {
+			this.stop();
+			this.start();
+		},
+		/**
+		 * Event that is invoked per iteration
+		 * @param {Function} setTickAction
+		 * @return {undefined}
+		 */
+		setTickAction: function (setTickAction) {
+			this.tickAction = setTickAction;
+		}
+	};
+}());
