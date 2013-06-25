778b0f6619c4bac362f404e96730c9a5124c4e90
Vivek Sharma <vivek.sharma@searshc.com>
2013-06-25 12:21:14 -0500

    [Login nav] - added check for objects when they are null and using intanceOf check for checking object type.

--- sears-partsdirect-common/src/main/java/com/spd/cq/searspartsdirect/common/tags/GetUserDataTag.java
+++ sears-partsdirect-common/src/main/java/com/spd/cq/searspartsdirect/common/tags/GetUserDataTag.java
@@ -65,20 +65,38 @@ public class GetUserDataTag extends CQBaseTag {
 			json = apiHelper.readJsonFromUrl(apiUrl.toString());
 			log.debug("json.toString() "+json.toString());
 			if (json.has("cart")) {
-				JSONObject cart = json.getJSONObject("cart");
-				JSONArray cartLinesArray = cart.getJSONArray("cartLines");
-				for (int i = 0; i < cartLinesArray.length(); i++) {
-					JSONObject cartLineObj = cartLinesArray.getJSONObject(i);
-					JSONObject partObj = cartLineObj.getJSONObject("part");
-					int qty = cartLineObj.getInt("quantity");
-					Part part = new Part(partObj.getString("partNumber"),
-							partObj.getString("productGroupId"),
-							partObj.getString("supplierId"));
-					CartLineModel cartLines = new CartLineModel(part, qty);
-					log.debug("cartLines2.toString() "+ cartLines.toString());
-					lstCartLines.add(cartLines);
+				Object cartObj = json.get("cart");
+					if (cartObj != null && cartObj instanceof JSONObject) {
+					JSONObject cart = json.getJSONObject("cart");
+					if (cart != null) {
+						Object cartLinesObj = cart.get("cartLines");
+						if (cartLinesObj instanceof JSONArray) {
+							JSONArray cartLinesArray = cart.getJSONArray("cartLines");
+							for (int i = 0; i < cartLinesArray.length(); i++) {
+								JSONObject cartLineObj = cartLinesArray.getJSONObject(i);
+								JSONObject partObj = cartLineObj.getJSONObject("part");
+								int qty = cartLineObj.getInt("quantity");
+								Part part = new Part(partObj.getString("partNumber"),
+										partObj.getString("productGroupId"),
+										partObj.getString("supplierId"));
+								CartLineModel cartLines = new CartLineModel(part, qty);
+								log.debug("cartLines2.toString() "+ cartLines.toString());
+								lstCartLines.add(cartLines);
+							}
+						} else if (cartLinesObj instanceof JSONObject) {
+							JSONObject cartLines = cart.getJSONObject("cartLines");
+							JSONObject partObject = cartLines.getJSONObject("part");
+							int qty = cartLines.getInt("quantity");
+							Part part = new Part(partObject.getString("partNumber"),
+									partObject.getString("productGroupId"),
+									partObject.getString("supplierId"));
+							CartLineModel model = new CartLineModel(part, qty);
+							lstCartLines.add(model);
+							log.debug("lstCartLines.toString() "+ lstCartLines.toString());
+						}
+					}	
+					pdUserDataModel.setShoppingCart(lstCartLines);
 				}
-				pdUserDataModel.setShoppingCart(lstCartLines);
 			}
 
 			//my profile models
