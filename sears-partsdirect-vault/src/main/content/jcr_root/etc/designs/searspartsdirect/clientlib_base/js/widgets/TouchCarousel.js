e20653a410e6e5e52090729bd935ff7b94de9010
Matt Lo <mlo@siteworx.com>
2013-06-26 13:16:02 -0500

    added image carousel widget and events

---
+++ sears-partsdirect-vault/src/main/content/jcr_root/etc/designs/searspartsdirect/clientlib_base/js/widgets/TouchCarousel.js
@@ -0,0 +1,17 @@
+NS('shc.pd.base.widgets').TouchCarousel = shc.pd.base.render.Breakpoint.extend(function () {
+	return {
+		/**
+		 * @constructor
+		 * @param {jQuery} parent {HTMLElement}
+		 */
+		init: function (parent) {
+			
+		},
+		activation: function () {
+			
+		},
+		deactivation: function () {
+			
+		}
+	};
+}());
