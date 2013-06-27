1ba752c289b700822e26e7092237fe1065d6c1f7
Benjamin Zethmayr <bzethmayr@siteworx.com>
2013-06-27 09:49:32 -0500

    Completes back-end for Maintenance Tips components

---
+++ sears-partsdirect-common/src/test/java/com/spd/cq/searspartsdirect/common/fixture/GetMaintenanceTopicJumpTagFixture.java
@@ -0,0 +1,100 @@
+package com.spd.cq.searspartsdirect.common.fixture;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.jcr.Node;
+import javax.jcr.PathNotFoundException;
+import javax.jcr.Property;
+import javax.jcr.RepositoryException;
+
+import org.apache.sling.api.resource.Resource;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import com.day.cq.wcm.api.Page;
+import com.spd.cq.searspartsdirect.common.tags.GetMaintenanceTopicJumpTag;
+
+import static org.mockito.Mockito.*;
+
+public class GetMaintenanceTopicJumpTagFixture {
+
+	private Page currentPage;
+	private Resource pageContent;
+	private List<Resource> pageChildren;
+	private List<Resource> parsysChildren;
+	
+	public GetMaintenanceTopicJumpTagFixture(Page currentPage) {
+		this.currentPage = currentPage;
+		pageContent = mock(Resource.class);
+		when(currentPage.getContentResource()).thenReturn(pageContent);
+		pageChildren = new ArrayList<Resource>();
+		when(pageContent.getChildren()).thenAnswer(new Answer<Iterable<Resource>>() {
+			public Iterable<Resource> answer(InvocationOnMock invocation)
+					throws Throwable {
+				return getPageChildren();
+			}
+		});
+	}
+	
+	public void setupNoContent() {
+		pageChildren = new ArrayList<Resource>();
+	}
+	
+	public void setupEmptyParsys() {
+		setupNoContent();
+		Resource parsys = getParsys();
+		pageChildren.add(parsys);
+		parsysChildren = new ArrayList<Resource>();
+		when(parsys.getChildren()).thenAnswer(new Answer<Iterable<Resource>>() {
+			public Iterable<Resource> answer(InvocationOnMock invocation)
+					throws Throwable {
+				return parsysChildren;
+			}
+		});
+	}
+	
+	public void setupOneTopic() throws PathNotFoundException, RepositoryException {
+		setupEmptyParsys();
+		pageChildren.add(getTopic("foo")); // wrong place, to cover a branch
+		parsysChildren.add(getTopic("foo"));
+		parsysChildren.add(getParsys()); // wrong place, to cover a branch
+	}
+	
+	public void setupTwoTopics() throws PathNotFoundException, RepositoryException {
+		setupOneTopic();
+		parsysChildren.add(getTopic("bar"));
+	}
+	
+	public void setupExplodingTopic() throws PathNotFoundException, RepositoryException {
+		setupEmptyParsys();
+		Resource aTopic = getTopic("foo");
+		parsysChildren.add(aTopic);
+		Node node = aTopic.adaptTo(Node.class);
+		when(node.getProperty(anyString())).thenThrow(new RepositoryException());
+	}
+	
+	public Resource getParsys() {
+		Resource parsys = mock(Resource.class);
+		when(parsys.getResourceType()).thenReturn(GetMaintenanceTopicJumpTag.PARSYS_TYPE);
+		when(parsys.getName()).thenReturn("parsys");
+		return parsys;
+	}
+
+	public Resource getTopic(String string) throws PathNotFoundException, RepositoryException {
+		Resource topic = mock(Resource.class);
+		when(topic.getResourceType()).thenReturn(GetMaintenanceTopicJumpTag.TOPIC_TYPE);
+		when(topic.getName()).thenReturn(string);
+		Node topicNode = mock(Node.class);
+		when(topic.adaptTo(Node.class)).thenReturn(topicNode);
+		Property titleProp = mock(Property.class);
+		when(topicNode.getProperty(GetMaintenanceTopicJumpTag.TITLE_PROP)).thenReturn(titleProp);
+		when(titleProp.getString()).thenReturn(string+string+string);
+		return topic;
+	}
+
+	private List<Resource> getPageChildren() {
+		return pageChildren;
+	}
+
+}
