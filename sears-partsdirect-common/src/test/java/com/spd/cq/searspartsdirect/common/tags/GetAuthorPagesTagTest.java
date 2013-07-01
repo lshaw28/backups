093614ba286e93551b311531e34b3ef640fef682
Benjamin Zethmayr <bzethmayr@siteworx.com>
2013-07-01 17:15:22 -0500

    Test case for GetAuthorPagesTag at 100/100 and some functionality asssertions

---
+++ sears-partsdirect-common/src/test/java/com/spd/cq/searspartsdirect/common/tags/GetAuthorPagesTagTest.java
@@ -0,0 +1,99 @@
+package com.spd.cq.searspartsdirect.common.tags;
+
+
+import java.util.List;
+
+import javax.jcr.RepositoryException;
+import javax.jcr.ValueFormatException;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.TagSupport;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.spd.cq.searspartsdirect.common.fixture.GetAuthorPagesTagFixture;
+import com.spd.cq.searspartsdirect.common.model.spdasset.AuthorModel;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class GetAuthorPagesTagTest extends MocksTag {
+
+	GetAuthorPagesTagFixture fixture;
+	GetAuthorPagesTag tag;
+	
+	@Before
+	protected void setUp() throws Exception {
+		super.setUp();
+		fixture = new GetAuthorPagesTagFixture(pageContext,pageManager,resourceResolver);
+		tag = new GetAuthorPagesTag();
+	}
+
+	@Test
+	public void testMinimusDoStartTag() throws JspException {
+		runsSkipsBodyEvalsPage();
+	}
+	
+	@Test
+	public void testWhenArrayHasOneAuthor() throws JspException, ValueFormatException, IllegalStateException, RepositoryException {
+		fixture.setUpAnAuthorInArray("jjj");
+		runsSkipsBodyEvalsPage();
+		@SuppressWarnings("unchecked")
+		List<AuthorModel> authors = (List<AuthorModel>)pageContext.getAttribute("authors");
+		assertThat(authors,hasSize(1));
+		AuthorModel author = authors.get(0);
+		assertThat(author.getPath(),is("jjj"));
+	}
+	
+	@Test
+	public void testWhenArrayHasOneAuthorEmptyImageAndAnAbstract() throws JspException, ValueFormatException, IllegalStateException, RepositoryException {
+		fixture.setUpAnAuthorInArray("lll");
+		fixture.setUpEmptyImage();
+		fixture.setUpAbstract("isisnt");
+		runsSkipsBodyEvalsPage();
+		@SuppressWarnings("unchecked")
+		List<AuthorModel> authors = (List<AuthorModel>)pageContext.getAttribute("authors");
+		assertThat(authors,hasSize(1));
+		AuthorModel author = authors.get(0);
+		assertThat(author.getPath(),is("lll"));
+		assertThat(author.getDescription(),is("isisnt"));
+	}
+	
+	@Test
+	public void testWhenArrayHasOneAuthorFileImage() throws JspException, ValueFormatException, IllegalStateException, RepositoryException {
+		fixture.setUpAnAuthorInArray("mmm");
+		fixture.setUpFileImage();
+		runsSkipsBodyEvalsPage();
+	}
+	
+	@Test
+	public void testWhenArrayHasOneAuthorFileReferenceImage() throws JspException, ValueFormatException, IllegalStateException, RepositoryException {
+		fixture.setUpAnAuthorInArray("nnn");
+		fixture.setUpFileReferenceImage();
+		runsSkipsBodyEvalsPage();
+	}
+	
+	@Test
+	public void testWhenExplodesVfe() throws JspException, ValueFormatException, IllegalStateException, RepositoryException {
+		fixture.setUpExplodesVfe();
+		runsSkipsBodyEvalsPage();
+	}
+	
+	@Test
+	public void testWhenExplodesPnfe() throws JspException, ValueFormatException, IllegalStateException, RepositoryException {
+		fixture.setUpExplodesPnfe();
+		runsSkipsBodyEvalsPage();
+	}
+	
+	@Test
+	public void testWhenExplodesRe() throws JspException, ValueFormatException, IllegalStateException, RepositoryException {
+		fixture.setUpExplodesRe();
+		runsSkipsBodyEvalsPage();
+	}
+
+	private void runsSkipsBodyEvalsPage() throws JspException {
+		tag.setPageContext(pageContext);
+		assertThat(tag.doStartTag(),is(TagSupport.SKIP_BODY));
+		assertThat(tag.doEndTag(),is(TagSupport.EVAL_PAGE));
+	}
+}
