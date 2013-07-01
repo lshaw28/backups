87e0e0adc630b8139a89721888bcb71f363e929d
Benjamin Zethmayr <bzethmayr@siteworx.com>
2013-07-01 09:02:49 -0500

    Test case was missing for GetNameByNodePathTag, added it

---
+++ sears-partsdirect-common/src/test/java/com/spd/cq/searspartsdirect/common/tags/GetNameByNodePathTagTest.java
@@ -0,0 +1,32 @@
+package com.spd.cq.searspartsdirect.common.tags;
+
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.TagSupport;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class GetNameByNodePathTagTest extends MocksTag {
+
+	GetNameByNodePathTag tag;
+	
+	@Before
+	protected void setUp() throws Exception {
+		super.setUp();
+		tag = new GetNameByNodePathTag();
+	}
+
+	@Test
+	public void testDoStartTag() throws JspException {
+		tag.setPageContext(pageContext);
+		tag.setNodePath("a/b/c/d");
+		assertThat(tag.doStartTag(),is(TagSupport.SKIP_BODY));	
+		assertThat(tag.doEndTag(),is(TagSupport.EVAL_PAGE));
+		assertThat((String)pageContext.getAttribute("nodeName"),is("d"));
+	}
+
+}
