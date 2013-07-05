54e82d5aa30e5da54a4659b46d79da655dabd2aa
Benjamin Zethmayr <bzethmayr@siteworx.com>
2013-07-05 15:33:09 -0500

    ParentPageTag test case to 100/100

--- sears-partsdirect-common/src/test/java/com/spd/cq/searspartsdirect/common/fixture/ParentPageTagFixture.java
+++ sears-partsdirect-common/src/test/java/com/spd/cq/searspartsdirect/common/fixture/ParentPageTagFixture.java
@@ -3,14 +3,16 @@ package com.spd.cq.searspartsdirect.common.fixture;
 import static org.mockito.Mockito.*;
 
 import com.day.cq.wcm.api.Page;
+import com.spd.cq.searspartsdirect.common.helpers.Constants;
 
 public class ParentPageTagFixture {
 
+	private Page currentPage;
 	private Page parentPage;
 	
 	public ParentPageTagFixture(Page currentPage) {
+		this.currentPage = currentPage;
 		parentPage = mock(Page.class);
-		when(currentPage.getParent()).thenReturn(parentPage);
 	}
 
 	public String getParent() {
@@ -18,11 +20,25 @@ public class ParentPageTagFixture {
 	}
 
 	public void setUpParent() {
+		attachParent();
 		when(parentPage.getPath()).thenReturn(getParent());
 	}
 
 	public void setUpNoParent() {
-		// We do not set up a path for the parent page here.
+		when(currentPage.getParent()).thenReturn((Page)null);
+	}
+
+	public void setUpNullPathParent() {
+		attachParent();
+		when(parentPage.getPath()).thenReturn((String)null);
 	}
 	
+	public void setUpEmptyPathParent() {
+		attachParent();
+		when(parentPage.getPath()).thenReturn(Constants.EMPTY);
+	}
+	
+	private void attachParent() {
+		when(currentPage.getParent()).thenReturn(parentPage);
+	}
 }
