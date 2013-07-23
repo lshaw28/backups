47f125dc5f5aedfcd270c3deb71355e698d97752
Benjamin Zethmayr <bzethmayr@siteworx.com>
2013-07-23 08:59:21 -0500

    Increases branch coverage for PagesByTagTag for 100/100 coverage

--- sears-partsdirect-common/src/test/java/com/spd/cq/searspartsdirect/common/tags/PagesByTagTagTest.java
+++ sears-partsdirect-common/src/test/java/com/spd/cq/searspartsdirect/common/tags/PagesByTagTagTest.java
@@ -6,6 +6,7 @@ import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.isA;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.Matchers.hasSize;
 
 import java.util.List;
 
@@ -42,6 +43,27 @@ public class PagesByTagTagTest extends MocksTag {
 			List<Page> pages = (List<Page>)pageContext.getAttribute("taggedPages");
 			assertThat(pages,not(nullValue()));
 			assertThat(pages,isA(List.class));
+			assertThat(pages,hasSize(2));
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	@Test
+	public void testTagUsageBadTag() {
+		try {
+			tag.setPageContext(pageContext);
+			tag.setTag(fixture.getTestTag());
+			fixture.breakTestTag();
+			int startResult = tag.doStartTag();
+			assertThat(startResult,is(TagSupport.SKIP_BODY));
+			int endResult = tag.doEndTag();
+			assertThat(endResult,is(TagSupport.EVAL_PAGE));
+			@SuppressWarnings("unchecked")
+			List<Page> pages = (List<Page>)pageContext.getAttribute("taggedPages");
+			assertThat(pages,nullValue()); // as it turns out, in this case we get null rather than empty
+			//assertThat(pages,isA(List.class));
+			//assertThat(pages,hasSize(0));
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
