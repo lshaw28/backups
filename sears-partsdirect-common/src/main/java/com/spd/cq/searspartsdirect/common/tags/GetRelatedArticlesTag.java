97d3ce9ab98ef8f7ca17c8af88f0fe77bd605f6e
Rajeev Chilamkurthy <rajeev.chilamkurthy@searshc.com>
2013-07-23 12:22:34 -0500

    [SPCM-273] - Removed 'Current Article' from the 'Related Articles' section.

--- sears-partsdirect-common/src/main/java/com/spd/cq/searspartsdirect/common/tags/GetRelatedArticlesTag.java
+++ sears-partsdirect-common/src/main/java/com/spd/cq/searspartsdirect/common/tags/GetRelatedArticlesTag.java
@@ -24,69 +24,55 @@ public class GetRelatedArticlesTag extends CQBaseTag {
 	private static final long serialVersionUID = 1L;
 	protected static Logger log = LoggerFactory.getLogger(GetRelatedArticlesTag.class);
 	protected String categoryPath;
-	
+
 	@Override
 	public int doStartTag() throws JspException {
-		
-		ArrayList<ArticleModel> articles = new ArrayList<ArticleModel>();
+
+		List<ArticleModel> articles = new ArrayList<ArticleModel>();
+
 		try {
 
 			List<Page> result = new ArrayList<Page>();
 
 			QueryBuilder qb = resourceResolver.adaptTo(QueryBuilder.class);
 			Map<String, String> props = new HashMap<String, String>();
-	        props.put("type", Constants.CQ_PAGE);
-	        props.put("path", Constants.ARTICLES_ROOT);
-	        props.put("property", Constants.ASSETS_PAGES_REL_PATH);
-	        props.put("property.value", categoryPath);
-	        
-	        List<Hit> hits = qb.createQuery(
-	        			PredicateGroup.create(props),resourceResolver.adaptTo(Session.class)
-	        		).getResult().getHits();
+			props.put("type", Constants.CQ_PAGE);
+			props.put("path", Constants.ARTICLES_ROOT);
+			props.put("property", Constants.ASSETS_PAGES_REL_PATH);
+			props.put("property.value", categoryPath);
 
-	        for (Hit hit: hits) {
-	        	result.add(pageManager.getPage(hit.getPath()));
-	        }
-	        
-	        Collections.sort(result, Collections.reverseOrder(new PageImpressionsComparator(resourceResolver)));
-	          
-	        String description = "";
-			if (result.size() <= 4){
-				for(Page page: result){
-					if(page.getProperties().containsKey("abstracttext")){
-						description = page.getProperties().get("abstracttext").toString();
-					} else {
-						description = "";
-					}
-					articles.add(new ArticleModel(
-							page.getPath() + ".html",
-							page.getPath() + Constants.ASSETS_IMAGE_PATH,
-							page.getTitle(),
-							description));
-				}
-			}
-			else {
-				for (int i=0; i < 4; i++){
-					if(result.get(i).getProperties().containsKey("abstracttext")){
-						description = result.get(i).getProperties().get("abstracttext").toString();
-					} else {
-						description = "";
-					}
-					articles.add(new ArticleModel(
-							result.get(i).getPath() + ".html",
-							result.get(i).getPath() + Constants.ASSETS_IMAGE_PATH,
-							result.get(i).getTitle(),
-							description));
+			List<Hit> hits = qb.createQuery(PredicateGroup.create(props),
+												resourceResolver.adaptTo(Session.class)).getResult().getHits();
+
+			for (Hit hit : hits) {
+				Page page = pageManager.getPage(hit.getPath());
+				if (!currentPage.equals(page))  {
+					result.add(page);
 				}
 			}
 
+			Collections.sort(result, Collections.reverseOrder(new PageImpressionsComparator(resourceResolver)));
+
+			String description = "";
+			for (int i = 0; i < result.size(); i++) {
+				if (result.get(i).getProperties().containsKey("abstracttext")) {
+					description = result.get(i).getProperties().get("abstracttext").toString();
+				} else {
+					description = "";
+				}
+				articles.add(new ArticleModel(result.get(i).getPath() + ".html",
+													result.get(i).getPath() + Constants.ASSETS_IMAGE_PATH,
+													result.get(i).getTitle(),
+													description));
+			}
+
+		} catch (Exception e) {
+			log.error("Failure building article list, ", e);
 		}
-		
-		catch (Exception e) {
-			log.error("Failure building article list, ",e);
-		}
-		pageContext.setAttribute("articles", articles);
-        return SKIP_BODY;
+
+		pageContext.setAttribute("articles", articles.subList(0, (articles.size() > 4 ? 4 : articles.size())));
+
+		return SKIP_BODY;
 	}
 
 	@Override
