151e4aa28f34de25cb59222da9537ac86e4da3c9
Rajeev Chilamkurthy <rajeev.chilamkurthy@searshc.com>
2013-07-25 12:42:24 -0500

    Performance tweaks. Use charAt(0) rather than startsWith('string constant') when the constant is a single character string. Using startsWith() with a one character argument works, but it makes several computations while preparing to compare its prefix with another string, which is unnecessary when comparing one character against another.

--- sears-partsdirect-common/src/main/java/com/spd/cq/searspartsdirect/common/tags/DisplayImageTag.java
+++ sears-partsdirect-common/src/main/java/com/spd/cq/searspartsdirect/common/tags/DisplayImageTag.java
@@ -19,58 +19,58 @@ import com.day.cq.wcm.foundation.Image;
 @SuppressWarnings("serial")
 public class DisplayImageTag extends CQBaseTag {
 	protected final static Logger log = LoggerFactory.getLogger(DisplayImageTag.class);
-	
+
 	protected String path;
 	protected boolean decorated = true;
 	protected String altText;
-	
+
 	@Override
 	public int doStartTag() throws JspException {
 		JspWriter out = pageContext.getOut();
 		Resource imgResource = resource;
-		if (path != null && path.startsWith("/")) {
+		if (path != null && (path.length() > 0 && path.charAt(0) == '/')) {
 			imgResource = resourceResolver.resolve(path);
 			path = null;
 		}
 		Image image = path == null ? new Image(imgResource) : new Image(imgResource,path);
-		
-	    //drop target css class = dd prefix + name of the drop target in the edit config
-	    image.addCssClass(DropTarget.CSS_CLASS_PREFIX + "image");
-	    image.loadStyleData(currentStyle);
-	    image.setSelector(".img"); // use image script
-	    //image.setDoctype(Doctype.fromRequest(request));
-	    // add design information if not default (i.e. for reference paras)
-	    if (!currentDesign.equals(resourceDesign)) {
-	        image.setSuffix(currentDesign.getId());
-	    }
-	    if (altText != null) {
-	    	image.setAlt(altText);
-	    }
-	    String divId = "cq-image-jsp-" + imgResource.getPath();
-	    try {
-	    	if (decorated) out.write("<div id=\"" + divId + "\">");
-	    	image.draw(out);
-	    	if (decorated) out.write("</div>");
-	    }
-	    catch (Exception e) {
-	    	log.error("drawing image, ",e);
-	    }
-        return SKIP_BODY;
+
+		//drop target css class = dd prefix + name of the drop target in the edit config
+		image.addCssClass(DropTarget.CSS_CLASS_PREFIX + "image");
+		image.loadStyleData(currentStyle);
+		image.setSelector(".img"); // use image script
+		//image.setDoctype(Doctype.fromRequest(request));
+		// add design information if not default (i.e. for reference paras)
+		if (!currentDesign.equals(resourceDesign)) {
+			image.setSuffix(currentDesign.getId());
+		}
+		if (altText != null) {
+			image.setAlt(altText);
+		}
+		String divId = "cq-image-jsp-" + imgResource.getPath();
+		try {
+			if (decorated) out.write("<div id=\"" + divId + "\">");
+			image.draw(out);
+			if (decorated) out.write("</div>");
+		}
+		catch (Exception e) {
+			log.error("drawing image, ",e);
+		}
+		return SKIP_BODY;
 	}
-	
+
 	@Override
-    public int doEndTag() throws JspException {
-        return EVAL_PAGE;
-    }
-	
+	public int doEndTag() throws JspException {
+		return EVAL_PAGE;
+	}
+
 	public void setPath(String path) {
 		this.path = path;
 	}
-	
+
 	public void setDecorated(boolean decorated) {
 		this.decorated = decorated;
 	}
-	
+
 	public void setAltText(String altText) {
 		this.altText = altText;
 	}
