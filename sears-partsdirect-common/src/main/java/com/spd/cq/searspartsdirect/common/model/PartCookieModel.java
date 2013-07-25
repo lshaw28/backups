4b1aa6e87e8a9f447027e7b6abd161d4e1fd3886
Rajeev Chilamkurthy <rajeev.chilamkurthy@searshc.com>
2013-07-25 11:23:21 -0500

    Added instanceof test in equals().

--- sears-partsdirect-common/src/main/java/com/spd/cq/searspartsdirect/common/model/PartCookieModel.java
+++ sears-partsdirect-common/src/main/java/com/spd/cq/searspartsdirect/common/model/PartCookieModel.java
@@ -1,74 +1,92 @@
 package com.spd.cq.searspartsdirect.common.model;
 
 public class PartCookieModel {
-	
-	public final static String recentlyViewedPartCookieName = "recentlyViewedParts";	
+
+	public final static String recentlyViewedPartCookieName = "recentlyViewedParts";
 	private String itemImageURL;
 	protected String itemName;
 	protected String itemDescription;
 	protected String itemURL;
-	
+
 	public String getItemName() {
 		return itemName;
 	}
+
 	public void setItemName(String itemName) {
 		this.itemName = itemName;
 	}
+
 	public String getItemURL() {
 		return itemURL;
 	}
+
 	public void setItemURL(String itemURL) {
 		this.itemURL = itemURL;
 	}
+
 	public String getItemDescription() {
 		return itemDescription;
 	}
+
 	public void setItemDescription(String itemDescription) {
 		this.itemDescription = itemDescription;
-	}	
+	}
+
 	public String getItemImageURL() {
 		return itemImageURL;
 	}
-	
+
 	public void setItemImageURL(String itemImageURL) {
 		this.itemImageURL = itemImageURL;
 	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result
-				+ ((itemDescription == null) ? 0 : itemDescription.hashCode());
-		result = prime * result
-				+ ((itemName == null) ? 0 : itemName.hashCode());
+		result = prime * result + ((itemDescription == null) ? 0 : itemDescription.hashCode());
+		result = prime * result + ((itemName == null) ? 0 : itemName.hashCode());
 		result = prime * result + ((itemURL == null) ? 0 : itemURL.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj)
+		if (this == obj) {
 			return true;
-		if (obj == null)
+		}
+		if (obj == null) {
 			return false;
-		if (getClass() != obj.getClass())
+		}
+		if (getClass() != obj.getClass()) {
 			return false;
+		}
+		if (!(obj instanceof PartCookieModel)) {
+			return false;
+		}
 		PartCookieModel other = (PartCookieModel) obj;
 		if (itemDescription == null) {
-			if (other.itemDescription != null)
+			if (other.itemDescription != null) {
 				return false;
-		} else if (!itemDescription.equals(other.itemDescription))
+			}
+		} else if (!itemDescription.equals(other.itemDescription)) {
 			return false;
+		}
 		if (itemName == null) {
-			if (other.itemName != null)
+			if (other.itemName != null) {
 				return false;
-		} else if (!itemName.equals(other.itemName))
+			}
+		} else if (!itemName.equals(other.itemName)) {
 			return false;
+		}
 		if (itemURL == null) {
-			if (other.itemURL != null)
+			if (other.itemURL != null) {
 				return false;
-		} else if (!itemURL.equals(other.itemURL))
+			}
+		} else if (!itemURL.equals(other.itemURL)) {
 			return false;
+		}
 		return true;
-	}		
+	}
 
 }
--- sears-partsdirect-common/src/main/java/com/spd/cq/searspartsdirect/common/model/spdasset/BrandModel.java
+++ sears-partsdirect-common/src/main/java/com/spd/cq/searspartsdirect/common/model/spdasset/BrandModel.java
@@ -1,13 +1,13 @@
 package com.spd.cq.searspartsdirect.common.model.spdasset;
 
 public class BrandModel {
-	
+
 	private String trueName;
 	private String title;
 	private String description;
 	private String logoPath;
 	private String path;
-	
+
 	public BrandModel(String trueName, String path, String title, String description, String logoPath) {
 		this.trueName = trueName;
 		this.title = title;
@@ -15,10 +15,11 @@ public class BrandModel {
 		this.logoPath = logoPath;
 		this.path = path;
 	}
-	
+
 	public String getTrueName() {
 		return trueName;
 	}
+
 	public void setTrueName(String trueName) {
 		this.trueName = trueName;
 	}
@@ -26,20 +27,23 @@ public class BrandModel {
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	
+
 	public String getLogoPath() {
 		return logoPath;
 	}
+
 	public void setLogoPath(String logoPath) {
 		this.logoPath = logoPath;
 	}
@@ -47,6 +51,7 @@ public class BrandModel {
 	public String getPath() {
 		return path;
 	}
+
 	public void setPath(String path) {
 		this.path = path;
 	}
@@ -55,10 +60,8 @@ public class BrandModel {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result
-				+ ((description == null) ? 0 : description.hashCode());
-		result = prime * result
-				+ ((logoPath == null) ? 0 : logoPath.hashCode());
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((logoPath == null) ? 0 : logoPath.hashCode());
 		result = prime * result + ((path == null) ? 0 : path.hashCode());
 		result = prime * result + ((title == null) ? 0 : title.hashCode());
 		return result;
@@ -66,33 +69,47 @@ public class BrandModel {
 
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj)
+		if (this == obj) {
 			return true;
-		if (obj == null)
+		}
+		if (obj == null) {
 			return false;
-		if (getClass() != obj.getClass())
+		}
+		if (getClass() != obj.getClass()) {
 			return false;
+		}
+		if (!(obj instanceof BrandModel)) {
+			return false;
+		}
 		BrandModel other = (BrandModel) obj;
 		if (description == null) {
-			if (other.description != null)
+			if (other.description != null) {
 				return false;
-		} else if (!description.equals(other.description))
+			}
+		} else if (!description.equals(other.description)) {
 			return false;
+		}
 		if (logoPath == null) {
-			if (other.logoPath != null)
+			if (other.logoPath != null) {
 				return false;
-		} else if (!logoPath.equals(other.logoPath))
+			}
+		} else if (!logoPath.equals(other.logoPath)) {
 			return false;
+		}
 		if (path == null) {
-			if (other.path != null)
+			if (other.path != null) {
 				return false;
-		} else if (!path.equals(other.path))
+			}
+		} else if (!path.equals(other.path)) {
 			return false;
+		}
 		if (title == null) {
-			if (other.title != null)
+			if (other.title != null) {
 				return false;
-		} else if (!title.equals(other.title))
+			}
+		} else if (!title.equals(other.title)) {
 			return false;
+		}
 		return true;
 	}
 }
