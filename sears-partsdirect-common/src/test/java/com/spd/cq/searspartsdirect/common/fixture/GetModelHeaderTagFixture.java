14582357ea9e261d68429328fa7c4429239f9420
Benjamin Zethmayr <bzethmayr@siteworx.com>
2013-07-25 15:34:06 -0500

    Improves test coverage for GetModelHeaderTag to 100/100 regardless of API connectivity

--- sears-partsdirect-common/src/test/java/com/spd/cq/searspartsdirect/common/fixture/GetModelHeaderTagFixture.java
+++ sears-partsdirect-common/src/test/java/com/spd/cq/searspartsdirect/common/fixture/GetModelHeaderTagFixture.java
@@ -1,6 +1,11 @@
 package com.spd.cq.searspartsdirect.common.fixture;
 
+import org.apache.sling.api.SlingHttpServletRequest;
+
 import com.spd.cq.searspartsdirect.common.environment.EnvironmentSettings;
+import com.spd.cq.searspartsdirect.common.helpers.ModelSubcomponentAPIHelper;
+import com.spd.cq.searspartsdirect.common.model.PDModelSubcomponentModel;
+import com.spd.cq.searspartsdirect.common.model.PDTab;
 import com.spd.cq.searspartsdirect.common.model.spdasset.BrandModel;
 import com.spd.cq.searspartsdirect.common.model.spdasset.ProductCategoryModel;
 
@@ -9,7 +14,10 @@ import static org.mockito.Mockito.when;
 
 public class GetModelHeaderTagFixture {
 
-	public GetModelHeaderTagFixture() throws Exception {
+	private SlingHttpServletRequest slingRequest;
+
+	public GetModelHeaderTagFixture(SlingHttpServletRequest slingRequest) throws Exception {
+		this.slingRequest = slingRequest;
 		new EnvironmentSettingsFixture().setUpRealDefaults(new EnvironmentSettings());
 	}
 	
@@ -27,5 +35,31 @@ public class GetModelHeaderTagFixture {
 		BrandModel testBrand = mock(BrandModel.class);
 		when(testBrand.getTitle()).thenReturn("Kenmore");
 		return testBrand;
+	}
+
+	public void setUpFakeCompleteResult() {
+		PDModelSubcomponentModel fakeResult = setUpFakeResult();
+		PDTab[] tabsArr = new PDTab[2];
+		when(fakeResult.getTabsArr()).thenReturn(tabsArr);
+		PDTab first = mock(PDTab.class);
+		when(first.getTabsName()).thenReturn("First");
+		when(first.getTabsLink()).thenReturn("tab:first");
+		tabsArr[0] = first;
+		PDTab last = mock(PDTab.class);
+		when(last.getTabsName()).thenReturn("Last");
+		when(last.getTabsLink()).thenReturn("tab:last");
+		tabsArr[1] = last;
+	}
+	
+	public void setUpFakeEmptyResult() {
+		PDModelSubcomponentModel fakeResult = setUpFakeResult();
+		PDTab[] tabsArr = new PDTab[0];
+		when(fakeResult.getTabsArr()).thenReturn(tabsArr);
+	}
+	
+	private PDModelSubcomponentModel setUpFakeResult() {
+		PDModelSubcomponentModel fakeResult = mock(PDModelSubcomponentModel.class);
+		when(slingRequest.getAttribute(ModelSubcomponentAPIHelper.MODELSUB_REQATTR)).thenReturn(fakeResult);
+		return fakeResult;
 	}
 }
