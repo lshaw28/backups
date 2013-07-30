1cb7aa5c6749a32e2a5151a669645fe59925e2ac
Dale Price <dprice@siteworx.com>
2013-07-30 10:22:03 -0500

    registerModal Form Submission Fix
    Added user.lastName field; populating successfulRegistrationURL field
    dynamically.

--- sears-partsdirect-vault/src/main/content/jcr_root/etc/designs/searspartsdirect/clientlib_base/js/components/guideNavigation.js
+++ sears-partsdirect-vault/src/main/content/jcr_root/etc/designs/searspartsdirect/clientlib_base/js/components/guideNavigation.js
@@ -9,18 +9,17 @@ var guideNavigation = Class.extend(function () {
 		init: function (el) {
 			// Parameters
 			this.el = el;
-			this.classOn = '';
-			this.classOff = '';
-			this.breakPoint = '';
-			this.dir = '';
+			this.classOn = 'gn-sticky';
+			this.classOff = 'gn-unsticky';
+			this.breakPoint = el.offset().top;
 			this.maxScroll = false;
-			this.callback = '';
+			// Bind Events
+			this.bindEvents();
 		},
 		/**
-		 * Given scroll position of parent window,
-		 * applies and removes "sticky" css classes
+		 * Given scroll position of parent window, applies and removes "sticky" css classes
 		 * @param {number} val Required current scrolltop of window
-		 *
+		 * @return {void}
 		 */
 		checkState: function(val) {
 			var self = this;
@@ -34,75 +33,26 @@ var guideNavigation = Class.extend(function () {
 			}
 		},
 		/**
-		 * returns bool value for set property
-		 * @return {bool}
-		 */
-		isset: function () {
-			var self=this;
-			if (self.el !== false) {
-				return true;
-			} else {
-				return false;
-			}
-		},
-		/**
 		 * sets max scrolling distance for
 		 * sticky nav functionality.
 		 * @param {int} max Optional maxscroll value
 		 */
 		maxScroll: function (max) {
-			var self=this;
+			var self = this;
 
 			self.maxScroll = max !== null ? maxScroll : false;
 		},
 		/**
-		 * sets breakpoint for
-		 * sticky nav functionality.
-		 * @param {string} breakpoint Optional css class name for sticky nav off
+		 * Bind events
+		 * @return {void}
 		 */
-		setBreakPoint: function (breakpoint) {
+		bindEvents: function () {
 			var self = this;
 
-			self.breakPoint = breakpoint !== null ? breakpoint : false;
-			if (self.breakPoint === false && self.el !== false) {
-			  self.breakPoint = self.el.offset()['top'];
-			}
-
-			//self.breakPoint = self.el.offset()['top'];
-		},
-		/**
-		 * sets direction for
-		 * sticky nav functionality.
-		 * NOTE: currently not used.
-		 * @param {string} dir Optional direction of scrolling
-		 */
-		setDir: function (dir) {
-			var self = this;
-			self.dir = dir !== null ? dir : 'top';
-		},
-		/**
-		 * specifies an optional callback fn
-		 * @param {string} callback Optional function to call on window scroll
-		 */
-		setCallback: function (callback) {
-			var self = this;
-			if (callback === null) {
-				callback = false;
-			}
-			if (callback !== false && typeof callback === "function") {
-				self.callback = callback;
-			}
-		},
-		/**
-		 * sets css class names to activate/deactivate
-		 * sticky nav functionality.
-		 * @param {string} classOn Optional css class name for sticky nav on
-		 * @param {string} classOff Optional css class name for sticky nav off
-		 */
-		setClassToggles: function (classOn, classOff) {
-			var self = this;
-			self.classOn = classOn !== null ? classOn : 'active';
-			self.classOff = classOff !== null ? classOff : 'inactive';
+			$(window).bind('scroll', function() {
+				var scrollDist = $(window).scrollTop();
+				self.checkState(scrollDist);
+			});
 		}
 	};
 }());
