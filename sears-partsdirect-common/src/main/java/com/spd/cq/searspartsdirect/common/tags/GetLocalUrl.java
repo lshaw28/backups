e51e140dacf0cf33ce5dd782045b0a6cce197884
Benjamin Zethmayr <bzethmayr@siteworx.com>
2013-07-31 17:49:31 -0500

    Adds settings for local instance host/port, http and https. Not yet added to xml configs.

---
+++ sears-partsdirect-common/src/main/java/com/spd/cq/searspartsdirect/common/tags/GetLocalUrl.java
@@ -0,0 +1,27 @@
+package com.spd.cq.searspartsdirect.common.tags;
+
+import javax.servlet.jsp.JspException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.spd.cq.searspartsdirect.common.environment.EnvironmentSettings;
+
+public class GetLocalUrl extends CQBaseTag {
+
+	private static final long serialVersionUID = 1L;
+	protected static Logger log = LoggerFactory.getLogger(GetLocalUrl.class);
+
+	@Override
+	public int doStartTag() throws JspException {
+		pageContext.setAttribute("nonSecureLocalUrl", "http://"+ EnvironmentSettings.getLocalHttpAndPort());
+		pageContext.setAttribute("secureLocalUrl", "https://"+ EnvironmentSettings.getLocalHttpsAndPort());
+		return SKIP_BODY;
+	}
+
+	@Override
+	public int doEndTag() throws JspException {
+		return EVAL_PAGE;
+	}
+
+}
