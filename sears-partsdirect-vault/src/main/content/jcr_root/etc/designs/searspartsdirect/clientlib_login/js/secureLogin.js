eef94c2af4f427d7e0ea40f7cbe2061e1934ea75
Dan McGovern <Daniel.McGovern@searshc.com>
2013-08-05 12:40:42 -0500

    Refactoring Register modal to work from secure iFrame

--- sears-partsdirect-vault/src/main/content/jcr_root/etc/designs/searspartsdirect/clientlib_login/js/secureLogin.js
+++ sears-partsdirect-vault/src/main/content/jcr_root/etc/designs/searspartsdirect/clientlib_login/js/secureLogin.js
@@ -4,7 +4,7 @@ var secureLogin = Class.extend(function () {
 
 	return {
 		/**
-		 * Initializes modalForm class
+		 * Initializes secureLogin class
 		 * Uses Regula validation
 		 * See documentation: https://github.com/vivin/regula/wiki
 		 */
