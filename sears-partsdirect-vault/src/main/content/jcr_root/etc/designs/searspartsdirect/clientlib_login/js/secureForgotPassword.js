945fb08d8ba87aacb5daf216d73104b2ad045a26
Dan McGovern <Daniel.McGovern@searshc.com>
2013-08-09 12:23:00 -0500

    [SPCM-371] Forgot password now working on qa, however additional testing of edge cases needed before it's ready for qa testing.

--- sears-partsdirect-vault/src/main/content/jcr_root/etc/designs/searspartsdirect/clientlib_login/js/secureForgotPassword.js
+++ sears-partsdirect-vault/src/main/content/jcr_root/etc/designs/searspartsdirect/clientlib_login/js/secureForgotPassword.js
@@ -1,144 +1,178 @@
 /*global window:true, $:true, Class:true, regula:true */
 var secureForgotPassword = Class.extend(function () {
-	"use strict";
+    "use strict";
 
-	return {
-		/**
-		 * Initializes secureForgotPassword class
-		 * Uses Regula validation
-		 * See documentation: https://github.com/vivin/regula/wiki
-		 */
-		init: function (el) {
-			this.el = el;
-			this.bindSubmit();
-			this.bindCancel();
-			this.resetFields();
-		},
-		/**
-		 * Binds the submit button to perform Regula validation
-		 * @return {void}
-		 */
-		bindSubmit: function () {
-			var self = this;
+    return {
+        /**
+         * Initializes secureForgotPassword class
+         * Uses Regula validation
+         * See documentation: https://github.com/vivin/regula/wiki
+         */
+        init: function (el) {
+            this.el = el;
+            this.bindSubmit();
+            this.bindCancel();
+            this.resetFields();
+        },
+        /**
+         * Binds the submit button to perform Regula validation
+         * @return {void}
+         */
+        bindSubmit: function () {
+            var self = this;
 
-			$('[data-submit=true]', self.el).bind('click', function (e) {
-				e.preventDefault();
-				self.validate();
-			});
-		},
-		/**
-		 * Validates the registration form and displays friendly errors
-		 * @return {void}
-		 */
-		validate: function () {
-			var self = this,
-				i = 0,
-				errorMessage = '',
-				divider = '',
-				regulaResponse = regula.validate();
+            $('[data-submit=true]', self.el).unbind('click').bind('click', function (e) {
+                e.preventDefault();
+                self.validate();
+            });
+        },
+        /**
+         * Validates the registration form and displays friendly errors
+         * @return {void}
+         */
+        validate: function () {
+            var self = this,
+                i = 0,
+                errorMessage = '',
+                divider = '',
+                regulaResponse = regula.validate();
 
-			// Parse the error messages
+            // Parse the error messages
 
-			for (i = 0; i < regulaResponse.length; i = i + 1) {
-				errorMessage += divider + regulaResponse[i].message;
-				divider = '<br />';
-			}
+            for (i = 0; i < regulaResponse.length; i = i + 1) {
+                errorMessage += divider + regulaResponse[i].message;
+                divider = '<br />';
+            }
 
-			// Populate the errors
-			$('.alert', self.el).html(errorMessage);
+            // Populate the errors
+            $('.alert', self.el).html(errorMessage);
 
-			// Display errors or submit the form
-			if (errorMessage.length > 0) {
-				$('.alert', self.el).removeClass('hidden');
-			} else {
+            // Display errors or submit the form
+            if (errorMessage.length > 0) {
+                $('.alert', self.el).removeClass('hidden');
+            } else {
 
-				var userName = $('[name=loginId]', self.el).val();
-				//var forgotPasswordService = 'https://www.searspartsdirect.com/partsdirect/ssoForgotPasswordEmailModal.action';
-				var forgotPasswordService = mainSitePath + '/partsdirect/ssoForgotPasswordEmailModal.action';
+                var userName = $('#fldEmail', self.el).val();
+                //var forgotPasswordService = 'https://www.searspartsdirect.com/partsdirect/ssoForgotPasswordEmailModal.action';
+                var forgotPasswordService = mainSitePathSecure + '/partsdirect/ssoForgotPasswordEmailModal.action?modal=true&forgotResponse=?';
 
-				self.submitForgotPassword( userName, forgotPasswordService);
-			}
-		},
-		/**
-		 * Binds cancel button to reset fields
-		 */
-		bindCancel: function () {
-			var self = this;
+                self.submitForgotPassword( userName, forgotPasswordService);
+            }
+        },
+        /**
+         * Binds cancel button to reset fields
+         */
+        bindCancel: function () {
+            var self = this;
 
-			$('[data-cancel]', self.el).bind('click', function () {
-				self.resetFields();
-				self.postMessage({ 'closeModal': '#forgotPasswordModal' });
-			});
-		},
+            $('[data-cancel]', self.el).bind('click', function () {
+                self.resetFields();
+                self.postMessage({ 'closeModal': '#forgotPasswordModal' });
+            });
+        },
 
-		resetFields: function () {
-			var self = this;
+        resetFields: function () {
+            var self = this;
 
-			$('.alert', self.el).addClass('hidden');
-			$('input', self.el).each(function() {
-				$(this).val('');
-			});
-		},
-		/**
-		 * Handles a successful callback
-		 */
-		successCallback: function (obj) {
-			var self = this;
-			console.log('forgotPassword success callback');
-			if (obj.Status === "success") {
-				// if successful, change h1 text and show success paragraph
-				$('h1', self.el).html('Email Sent');
-				$('.forgot-success', self.el).removeClass('hidden');
-				// also, hide fieldset, old paragraph and cancel button.
-				$('forgot-intro', self.el).addClass('hidden');
-				$('fieldset', self.el).addClass('hidden');
-				$('[data-cancel]', self.el).addClass('hidden');
-				// finally, change continue button action to close modal
-				$('[data-submit=true]', self.el).bind('click', function (e) {
-					self.resetFields();
-					self.postMessage({ 'closeModal': '#forgotPasswordModal' });
-				});
-			} else {
-				console.log(obj.Status+" is what came back from forgotPassword service");
-				// display error message that email was not found
-				var errorMsg = "We are sorry. We cannot find the email address you entered. Please verfiy and enter again.";
-				$('.alert', self.el).html(errorMessage);
-				$('.alert', self.el).removeClass('hidden');
-			}
-		},
+            $('.alert', self.el).addClass('hidden');
+            $('input', self.el).each(function() {
+                $(this).val('');
+            });
+            // hide the success msg, show the intro msg, form, and cancel btn
+            $('.forgot-intro', self.el).removeClass('hidden');
+            $('fieldset', self.el).removeClass('hidden');
+            $('[data-cancel]', self.el).removeClass('hidden');
+            $('.forgot-success', self.el).addClass('hidden');
+        },
+        /**
+         * Handles a successful callback
+         */
+        showSuccess: function () {
+            var self = this;
 
-		errorCallback: function (errors) {
-			var self = this;
-		},
+            $('.alert', self.el).addClass('hidden');
+            // successful...change h1 text and show success paragraph
+            $('h1', self.el).html('Email Sent');
+            $('.forgot-success', self.el).removeClass('hidden');
+            // also, hide fieldset, old paragraph and cancel button.
+            $('.forgot-intro', self.el).addClass('hidden');
+            $('fieldset', self.el).addClass('hidden');
+            $('[data-cancel]', self.el).addClass('hidden');
+            // finally, change continue button action to close modal
+            $('[data-submit=true]', self.el).unbind('click').bind('click', function (e) {
+                self.resetFields();
+                self.postMessage({ 'closeModal': '#forgotPasswordModal' });
+            });
 
-		submitForgotPassword: function(username, forgotPasswordURL) {
-			var self = this;
+        },
+        showError: function (errorMsg) {
+            var self = this;
 
-			$.ajax({
-				type: "GET",
-				async: false,
-				contentType: 'application/json',
-				dataType: 'JSON',
-				url: forgotPasswordURL,
-				data: { userName: username},
-				success: self.successCallback,
-				error: self.errorCallback
-			});
-			return false;
-		},
+            // Populate the alert field with the errors
+            $('.alert', self.el).html(errorMsg);
+            $('.alert', self.el).removeClass('hidden');
+            // blank out input fields
+            $('input', self.el).each(function() {
+                $(this).val('');
+            });
 
-		/**
-		 * Posts a message to the parent page via JavaScript
-		 * @param {object} message The object to post
-		 */
-		postMessage: function (message) {
-			var domain = document.location.protocol + '//' + document.location.hostname + ':' + document.location.port;
+        },
 
-			if (typeof window['parentDomain'] === 'string') {
-				domain = window['parentDomain'];
-			}
+        submitForgotPassword: function(userEmail, forgotPasswordURL) {
+            var self = this,
+            notFoundErrorMsg = "We are sorry. We cannot find the email address you entered. Please verify and enter again.",
+            errorHappenedMsg = "Our records show you’re a member of our commercial parts website. We’re automatically redirecting you to Sears Commercial Parts.",
+            casErrorMsg = "Cas service problem. Can't find such user",
+            commericalURL = "partscommercialvip.qa.ch3.s.com";
 
-			top.window.postMessage(message, domain);
-		}
-	};
+            function forgotResponse(data, textStatus, jqXHR) {
+                console.log(data);
+
+                switch (data.Status) {
+
+                    case "success":
+                        self.showSuccess();
+                        break;
+                    case "notregister":
+                        self.showError(notFoundErrorMsg)
+                        break;
+                    case "error":
+                        self.showError(errorHappenedMsg);
+                        setTimeout(function(){window.location.href = commericalURL+"/partsdirect/commercialLogin.pd?email=" + $('#fldEmail').val()}, 3000);
+                        break;
+                    case "caserror":
+                        self.showError(casErrorMsg);
+                        break;
+                }
+            };
+
+            var ajx = $.ajax({
+                type: "GET",
+                async: false,
+                dataType: 'jsonp',
+                url: forgotPasswordURL,
+                data: { email: userEmail},
+                jsonpCallback: 'forgotResponse'
+            });
+
+            ajx.done(forgotResponse);
+
+            return false;
+
+        },
+
+        /**
+         * Posts a message to the parent page via JavaScript
+         * @param {object} message The object to post
+         */
+        postMessage: function (message) {
+            var domain = document.location.protocol + '//' + document.location.hostname + ':' + document.location.port;
+
+            if (typeof window['parentDomain'] === 'string') {
+                domain = window['parentDomain'];
+            }
+
+            top.window.postMessage(message, domain);
+        }
+    };
 }());
