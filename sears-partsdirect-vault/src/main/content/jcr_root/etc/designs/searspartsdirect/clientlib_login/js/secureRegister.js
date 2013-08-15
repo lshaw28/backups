12fcfefd2c47b3e2cb2a18e8ac501e0b1c7d6c8a
Dan McGovern <Daniel.McGovern@searshc.com>
2013-08-15 15:47:57 -0500

    changing redirect url for register modal

--- sears-partsdirect-vault/src/main/content/jcr_root/etc/designs/searspartsdirect/clientlib_login/js/secureRegister.js
+++ sears-partsdirect-vault/src/main/content/jcr_root/etc/designs/searspartsdirect/clientlib_login/js/secureRegister.js
@@ -57,8 +57,6 @@ var secureRegister = Class.extend(function () {
                 var hostName = window.SPDUtils.getLocationDetails().fullAddress,
                     tempRedirectURL = hostName+'content/searspartsdirect/en/login_form.html?authSuccessURL=true#'+window.parentDomain;
 
-                //tempRedirectURL = "/index.pd?authSuccessURL=true#"+window.parentDomain;
-                tempRedirectURL = "http://www.google.com";
 
                 // set the hidden form redirect url values
                 $('#currentPageURL').val(tempRedirectURL);
