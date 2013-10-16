4f789eafecc0408988dd30c9646e366e69b21b4d
Dale Price <dprice@siteworx.com>
2013-10-16 14:37:39 -0500

    More responsivePinchImage JS Improvements And A Minor Style Tweak
    Correct styling to come.

--- sears-partsdirect-vault/src/main/content/jcr_root/etc/designs/searspartsdirect/clientlib_base/js/components/responsivePinchImage.js
+++ sears-partsdirect-vault/src/main/content/jcr_root/etc/designs/searspartsdirect/clientlib_base/js/components/responsivePinchImage.js
@@ -23,7 +23,6 @@ var responsivePinchImage = Class.extend(function () {
 			this.imageHeight = 0;
 			// Last Event Scale
 			this.lastScale = 1;
-			this.transformPrefix = null;
 			// Perform setup
 			this.getProperties();
 			if (this.hasImage === true) {
@@ -46,27 +45,6 @@ var responsivePinchImage = Class.extend(function () {
 			self.mobileImage = su.validString(self.container.data('mobileimage'));
 			if (self.desktopImage !== '' || self.tabletImage !== '' || self.mobileImage !== '') {
 				self.hasImage = true;
-			}
-			// Set vendor prefix for CSS transforms
-			self.setPrefix();
-		},
-		/**
-		 * Determines the vendor prefix to use for CSS transformations
-		 * If none is supported, retain the null value for future logic
-		 * @returns {void}
-		 */
-		setPrefix: function () {
-			var self = this,
-				div = $('<div />'),
-				i,
-				list = ['transform', 'msTransform', 'MozTransform', 'WebkitTransform', 'OTransform'];
-
-			// Determine which prefix to use
-			for (i = 0; i < list.length; ++i) {
-				if (typeof div[0].style[list[i]] !== 'undefined') {
-					self.transformPrefix = list[i];
-					return;
-				}
 			}
 		},
 		/**
@@ -101,17 +79,15 @@ var responsivePinchImage = Class.extend(function () {
 				self.image.attr('src', newImage);
 				// Reset image scale
 				self.lastScale = 1;
-				if (self.transformPrefix !== null) {
-					self.image[0].style[self.transformPrefix] = '';
-				}
 				// Reset initial CSS
 				self.image.css({
 					'height': 'auto',
 					'left': '50%',
 					'top': '50%',
-					'width': '100%'
+					'width': 'auto'
 				});
 				// Set image load CSS
+				// Force pixel sizes and negative margins for centering
 				self.image.bind('load', function () {
 					self.imageWidth = self.image.width();
 					self.imageHeight = self.image.height();
@@ -119,7 +95,8 @@ var responsivePinchImage = Class.extend(function () {
 					self.image.css({
 						'height': self.imageHeight,
 						'margin-left': 0 - self.imageWidth / 2,
-						'margin-top': 0 - self.imageHeight / 2
+						'margin-top': 0 - self.imageHeight / 2,
+						'width': self.imageWidth
 					});
 				});
 			}
@@ -170,14 +147,22 @@ var responsivePinchImage = Class.extend(function () {
 
 			// Bind pinch events based on the availability of touchstart
 			if (isMobile === true) {
-				self.image.hammer().on('pinchin', function(event) {
-					event.preventDefault();
+				self.image.hammer({
+					'prevent_default': true
+				}).on('pinchin', function(event) {
+					event.gesture.preventDefault();
 					self.handleSizing(event);
 				}).on('pinchout', function(event) {
-					event.preventDefault();
+					event.gesture.preventDefault();
 					self.handleSizing(event);
 				});
 			}
+			self.image.hammer({
+				'prevent_default': true
+			}).on('drag', function (event) {
+				event.gesture.preventDefault();
+				self.handleDrag(event);
+			});
 			// Bind button clicks
 			self.plusButton.bind('click', function () {
 				var clientPos = self.getCenter();
@@ -246,17 +231,51 @@ var responsivePinchImage = Class.extend(function () {
 			self.imageWidth = newWidth;
 			self.imageHeight = newHeight;
 			
-			// Scale the image using transforms or regular old math
-			if (self.transformPrefix !== null) {
-				self.image[0].style[self.transformPrefix] = 'scale3d(' + scale + ', ' + scale + ', 1)';
-			} else {
-				self.image.css({
-					'height': self.imageHeight,
-					'margin-left': 0 - self.imageWidth / 2,
-					'margin-top': 0 - self.imageHeight / 2,
-					'width': self.imageWidth
-				});
+			// Scale the image
+			self.image.stop().animate({
+				'height': self.imageHeight,
+				'margin-left': 0 - self.imageWidth / 2,
+				'margin-top': 0 - self.imageHeight / 2,
+				'width': self.imageWidth
+			}, 50);
+		},
+		/**
+		 * Handles a drag event
+		 * @param {object} event Event fired by user or JavaScript
+		 */
+		handleDrag: function (event) {
+			var self = this,
+				isMobileBrowser = window.SPDUtils.isMobileBrowser(),
+				moveX = event.gesture.deltaX,
+				moveY = event.gesture.deltaY,
+				containerWidth = self.container.width(),
+				containerHeight = self.container.height(),
+				marginLeft = parseInt(self.image.css('margin-left').replace('px', ''), 10),
+				marginTop = parseInt(self.image.css('margin-top').replace('px', ''), 10);
+
+			// Handle different drag directions
+			moveX = moveX / 10;
+			moveY = moveY / 10;
+			marginLeft = marginLeft + moveX;
+			marginTop = marginTop + moveY;
+			
+			// Sanitise
+			if (marginLeft + self.imageWidth <= 0) {
+				marginLeft += 10;
+			} else if (marginLeft + self.imageWidth >= containerWidth) {
+				marginLeft -= 10;
 			}
+			if (marginTop + self.imageHeight <= 0) {
+				marginTop += 10;
+			} else if (marginTop + self.imageHeight >= containerHeight) {
+				marginTop -= 10;
+			}
+
+			// Set new margins
+			self.image.stop().animate({
+				'margin-left': marginLeft,
+				'margin-top': marginTop
+			}, 50);
 		}
 	};
 }());
