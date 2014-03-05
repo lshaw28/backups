f73f227ee54abc5cdddee7181f21333def4ed918
Jiju Varghese <jiju.varghese@searshc.com>
2014-03-05 16:28:34 -0600

    [Mingle #7767] - Creating sitemap for dynamic pages in CQ5 – thousands of new URLs for google index

--- sears-partsdirect-common/src/main/java/com/spd/cq/searspartsdirect/common/helpers/CategorySiteMapHelper.java
+++ sears-partsdirect-common/src/main/java/com/spd/cq/searspartsdirect/common/helpers/CategorySiteMapHelper.java
@@ -323,7 +323,8 @@ public class CategorySiteMapHelper implements Runnable {
                    sitemapEntryStr.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
                    sitemapEntryStr
                    .append("<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd\">");
-               fileStart = Boolean.FALSE;
+                   sitemapEntryStr.append(System.getProperty("line.separator"));
+                   fileStart = Boolean.FALSE;
                }
                 JSONObject  jsonObject = jsonObjects.get(i);
                  String brand = jsonObject.get("brandName").toString().trim();
